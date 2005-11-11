17a5b8fbfb23b687911a1f49cef1d972c274f325
Niels Lohmann <niels.lohmann@gmail.com>
2005-11-11 15:52:55 +0000

    + calls helper function "intToString"

--- bpel2owfn/src/debug.h
+++ bpel2owfn/src/debug.h
@@ -15,14 +15,13 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "helpers.h"
 
 #define TRACE_ALWAYS 0
 #define TRACE_WARNINGS 1
 #define TRACE_INFORMATION 2
 #define TRACE_DEBUG 3
 #define TRACE_VERY_DEBUG 4
-
-extern std::string intToString(int i);
 
 void trace(int trace_level, std::string message);
 void trace(std::string message);

0ba423ba2e3bcde73ade35ae31ba5dbb6fa714e1
Niels Lohmann <niels.lohmann@gmail.com>
2005-11-15 16:17:47 +0000

    + added exception-codes for PetriNet-class

--- bpel2owfn/src/exception.h
+++ bpel2owfn/src/exception.h
@@ -10,18 +10,18 @@
  * 
  * \author  
  *          - responsible: Christian Gierds <gierds@informatik.hu-berlin.de>
- *          - last changes of: \$Author: gierds $
+ *          - last changes of: \$Author: nlohmann $
  *          
  * \date
  *          - created: 2005/11/15
- *          - last changed: \$Date: 2005/11/15 15:47:23 $
+ *          - last changed: \$Date: 2005/11/15 16:17:47 $
  * 
  * \note    This file is part of the tool BPEL2oWFN and was created during the
  *          project "Tools4BPEL" at the Humboldt-Universität zu Berlin. See
  *          http://www.informatik.hu-berlin.de/top/forschung/projekte/tools4bpel
  *          for details.
  *
- * \version \$Revision: 1.2 $
+ * \version \$Revision: 1.3 $
  *          - 2005-11-11 (gierds) Initial version.
  *
  */
@@ -30,7 +30,9 @@
 
 typedef enum
 {
-  FILE_NOT_FOUND = 2	/// "File not found" exception
+  FILE_NOT_FOUND = 2,	/// "File not found" exception
+  NODE_NOT_FOUND,       /// "Node not found" exception (petrinet.cc)
+  MERGING_ERROR         /// "Merging-error" exception (petrinet.cc)
 } exception_id;
 
 /**

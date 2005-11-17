fdd55e44c1fba1574d21c2b3351009b7f8724d69
reinert <reinert_mail@example.com>
2005-11-17 13:13:11 +0000

    minor changes

--- bpel2owfn/src/bpel-attributes.cc
+++ bpel2owfn/src/bpel-attributes.cc
@@ -1,3 +1,27 @@
+/*!
+ * \file bpel-attributes.c
+ *
+ * \brief Functions for Petri nets (implementation)
+ *
+ * This file implements the classes and functions defined in bpel-attributes.h.
+ * 
+ * \author  
+ *          - responsible: Niels Lohmann <nlohmann@informatik.hu-berlin.de>
+ *          - last changes of: \$Author: reinert $
+ *          
+ * \date
+ *          - created: 2005/10/18
+ *          - last changed: \$Date: 2005/11/17 13:13:11 $
+ * 
+ * \note    This file is part of the tool BPEL2oWFN and was created during the
+ *          project "Tools4BPEL" at the Humboldt-Universit� zu Berlin. See
+ *          http://www.informatik.hu-berlin.de/top/forschung/projekte/tools4bpel
+ *          for details.
+ *
+ * \version \$Revision: 1.4 $
+ *
+ */
+
 #include "bpel-attributes.h"
 
 

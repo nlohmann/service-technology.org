92c720b2992ed8a5573383bbbdab0e7c09036fe5
reinert <reinert_mail@example.com>
2006-01-12 14:35:19 +0000

    symbol table dummy

---
+++ bpel2owfn/src/symbol-table.h
@@ -0,0 +1,53 @@
+/*****************************************************************************\
+ * Copyright 2005, 2006 Niels Lohmann, Christian Gierds, Dennis Reinert      *
+ *                                                                           *
+ * This file is part of BPEL2oWFN.                                           *
+ *                                                                           *
+ * BPEL2oWFN is free software; you can redistribute it and/or modify it      *
+ * under the terms of the GNU General Public License as published by the     *
+ * Free Software Foundation; either version 2 of the License, or (at your    *
+ * option) any later version.                                                *
+ *                                                                           *
+ * BPEL2oWFN is distributed in the hope that it will be useful, but WITHOUT  *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for  *
+ * more details.                                                             *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License along   *
+ * with BPEL2oWFN; if not, write to the Free Software Foundation, Inc., 51   *
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.                      *
+\****************************************************************************/ 
+
+/*!
+ * \file symbol-table.h
+ *
+ * \brief
+ *
+ * \author  
+ *          
+ * \date
+ * 
+ * \note    This file is part of the tool BPEL2oWFN and was created during the
+ *          project "Tools4BPEL" at the Humboldt-Universit�t zu Berlin. See
+ *          http://www.informatik.hu-berlin.de/top/forschung/projekte/tools4bpel
+ *          for details.
+ *
+ * \version \$Revision: 
+ *
+ */
+
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+class SymbolTable
+{
+  public:
+    /// constructor
+    SymbolTable();
+    
+    /// destructor
+    ~SymbolTable();
+
+};
+
+#endif

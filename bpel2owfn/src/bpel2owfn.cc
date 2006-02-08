1dc7409973a7204d6952dcaa768093f6a5ced134
Chris <gierds@informatik.hu-berlin.de>
2006-02-08 11:34:11 +0000

    * changed behaviour of Petri net info output (triggered via -finfo) in case of an exception

--- bpel2owfn/src/bpel2owfn.cc
+++ bpel2owfn/src/bpel2owfn.cc
@@ -32,14 +32,14 @@
  *          
  * \date
  *          - created: 2005/10/18
- *          - last changed: \$Date: 2006/02/07 13:36:30 $
+ *          - last changed: \$Date: 2006/02/08 11:34:11 $
  * 
  * \note    This file is part of the tool BPEL2oWFN and was created during the
  *          project "Tools4BPEL" at the Humboldt-Universität zu Berlin. See
  *          http://www.informatik.hu-berlin.de/top/forschung/projekte/tools4bpel
  *          for details.
  *
- * \version \$Revision: 1.53 $
+ * \version \$Revision: 1.54 $
  *          - 2005-11-15 (gierds) Moved command line evaluation to helpers.cc.
  *            Added option to created (abstracted) low level nets.
  *            Added option for LoLA output.
@@ -280,8 +280,9 @@ int main( int argc, char *argv[])
   catch (Exception& e)
   {
     // output info file ?
-    if ( modus == M_PETRINET )
+    if ( formats[F_INFO] )
     {
+      output = log_output;
       // just print to std::out
       TheNet->printInformation();
     }

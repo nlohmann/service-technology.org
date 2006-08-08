cd3d4992c120bd609354a6ae14adaa152f99fcdd
kaschner <kaschner_mail@example.com>
2006-08-08 12:38:40 +0000

    *** empty log message ***

---
+++ fiona/src/BddLabel.cc
@@ -0,0 +1,66 @@
+#include "BddLabel.h"
+#include <string>
+
+BddLabel::BddLabel(char * c, int n, BddLabelTab * table){
+	name = c;
+	nbr = n;	
+	next = (BddLabel *) 0;
+	table->add(this);
+}
+
+BddLabel::~BddLabel()
+{}
+
+BddLabelTab::BddLabelTab(unsigned int s){
+	size = s;
+	table = new BddLabel * [s];
+	card = 0;
+	for(int i = 0; i < s;i++){
+		table[i] = (BddLabel *) 0;
+	}
+}
+
+BddLabelTab::~BddLabelTab(){
+	for(int i = 0;i < size;i++){
+		BddLabel * temp;
+		while(table[i]){
+			temp = table[i];
+			table[i] = table[i] -> next;
+			delete temp;
+		}	
+	}
+	delete [] table;
+}
+
+	
+BddLabel * BddLabelTab::lookup(char * name){
+  	/* 1. Hashvalue bestimmen */
+  	unsigned int h,i;
+  	BddLabel * lst;
+  	h = 0;
+  	for(i = 0; i < strlen(name); i++){
+      	h += (int) name[i];
+      	h %= size;
+   	}
+  	/* 2. suchen */
+  	for(lst = table[h]; lst; lst = lst -> next){
+  		if(!strcmp(lst->name, name)){
+	   		break;
+	 	}
+    }
+  	return lst;
+}
+  
+void BddLabelTab::add(BddLabel * s){
+  	/* 1. Hashvalue bestimmen */
+  	unsigned int h,i;
+  	h = 0;
+  	for(i = 0; i < strlen(s->name); i++){
+      	h += (int) s->name[i];
+      	h %= size;
+    }
+  	/* 2. eintragen */
+  	s->next = table[h];
+  	table[h] = s;
+  	card++;
+}
---
+++ fiona/src/BddLabel.h
@@ -0,0 +1,42 @@
+#ifndef BDDLABEL_H_
+#define BDDLABEL_H_
+
+#include "mynew.h" 
+
+class BddLabelTab;
+
+class BddLabel{
+public:
+  char * name;	//name of Label
+  int nbr;		//number of Label
+  BddLabel * next;
+  BddLabel(char * c, int n, BddLabelTab * table);
+  ~BddLabel();
+
+// Provides user defined operator new. Needed to trace all new
+// operations on this class.
+#undef new
+        NEW_OPERATOR(BddLabel)
+#define new NEW_NEW  
+  
+};
+
+
+class BddLabelTab{
+	public:
+	BddLabel ** table;
+	BddLabel * lookup(char *);
+	void add(BddLabel *);
+	BddLabelTab(unsigned int size = 65536);
+  	~BddLabelTab();
+	unsigned int card;
+	unsigned int size;
+
+// Provides user defined operator new. Needed to trace all new
+// operations on this class.
+#undef new
+        NEW_OPERATOR(BddLabelTab)
+#define new NEW_NEW	
+};
+
+#endif /*BDDLABEL_H_*/

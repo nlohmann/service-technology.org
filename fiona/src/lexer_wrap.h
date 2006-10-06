5563488c49879814a139cc34bb906df52ce6e769
bretschn <bretschn_mail@example.com>
2006-10-06 15:03:37 +0000

    added comment to explain use of lexer_wrap.h

--- fiona/src/lexer_wrap.h
+++ fiona/src/lexer_wrap.h
@@ -1,5 +1,9 @@
 #include "lexer.h"
 
+// #defines YY_FLEX_HAS_YYLEX_DESTROY for newer versions of flex. If
+// YY_FLEX_HAS_YYLEX_DESTROY is defined, you should call yylex_destroy() after
+// parsing is complete.
+
 #if (YY_FLEX_MAJOR_VERSION > 2) || \
     ((YY_FLEX_MAJOR_VERSION == 2) && (YY_FLEX_MINOR_VERSION > 5)) || \
     ((YY_FLEX_MAJOR_VERSION == 2) && (YY_FLEX_MINOR_VERSION == 5) && \

9ab276c96d7b07ce46f371caa8c64e4ca5f3a405
Niels Lohmann <niels.lohmann@gmail.com>
2006-10-24 12:39:17 +0000

    + since I renamed some files, I had to change some others

--- bpel2owfn/src/cfg.cc
+++ bpel2owfn/src/cfg.cc
@@ -31,14 +31,14 @@
  *          
  * \date
  *          - created: 2006-01-19
- *          - last changed: \$Date: 2006/10/23 12:04:04 $
+ *          - last changed: \$Date: 2006/10/24 12:39:17 $
  * 
  * \note    This file is part of the tool BPEL2oWFN and was created during the
  *          project "Tools4BPEL" at the Humboldt-Universität zu Berlin. See
  *          http://www.informatik.hu-berlin.de/top/forschung/projekte/tools4bpel
  *          for details.
  *
- * \version \$Revision: 1.33 $
+ * \version \$Revision: 1.34 $
  *
  * \todo    - commandline option to control drawing of clusters 
  */
@@ -49,7 +49,7 @@
 #include "cfg.h"
 #include "options.h"
 #include "debug.h"
-#include "ast-printers.h"
+#include "ast-output.h"
 #include "ast-details.h"
 
 extern map<unsigned int, map<string, string> > temporaryAttributeMap;

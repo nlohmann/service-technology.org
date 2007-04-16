63f1f3111bbac7e35f142cd82e9670ffc6f32630
kaschner <kaschner_mail@example.com>
2007-04-16 14:46:48 +0000

    Testfälle für BDDs hinzugefügt (Im Ordner bdd_ref sind die BDDs gespeichert, mit denen die Vergleiche während der Tests durchgeführt werden)

--- fiona/tests/bddtest.sh
+++ fiona/tests/bddtest.sh
@@ -35,7 +35,7 @@ FIONA=fiona
 #loeschen aller erzeugten Dateien im letzten Durchlauf
 rm -f $DIR/*.out
 rm -f $DIR/*.png
-rm -f $DIR/*.cudd
+#rm -f $DIR/*.cudd
 rm -f $DIR/*.og
 rm -f $DIR/*.log
 
@@ -66,6 +66,137 @@ else
       result=1
     fi
 fi
+
+############################################################################
+
+nodes_mp=28
+nodes_ann=15
+
+owfn="$DIR/sequence3.owfn"
+cmd="$FIONA -n $owfn -a -b 4 -t OG"
+
+if [ "$memcheck" = "yes" ]; then
+    memchecklog="$owfn.a.b4.IG.memcheck.log"
+    do_memcheck "$cmd" "$memchecklog"
+    result=$?
+else
+    echo running $cmd
+    OUTPUT=`$cmd 2>&1`
+    echo $OUTPUT | grep "BDD_MP: number of nodes: $nodes_mp" > /dev/null
+    resultNodesMP=$?
+    echo $OUTPUT | grep "BDD_ANN: number of nodes: $nodes_ann" > /dev/null
+    resultNodesANN=$?
+    
+    if [ $resultNodesMP -ne 0 ] ; then
+        echo     ... FAILED
+        echo Number of nodes of BDD_MP is incorrect.
+        echo
+        result=1
+    fi
+
+    if [ $resultNodesANN -ne 0 ] ; then
+        echo     ... FAILED
+        echo Number of nodes of BDD_ANN is incorrect.
+        echo
+        result=1
+    fi
+
+fi
+
+############################################################################
+
+owfn="$DIR/sequence3.owfn"
+owfn_reference="$testdir/bdd_ref/sequence3_reference.owfn"
+cmd="$FIONA -n $owfn -a -b 4 -t OG"
+$cmd 2>/dev/null 1>/dev/null
+
+cmd="$FIONA -n $owfn -n $owfn_reference -x"
+
+
+if [ "$memcheck" = "yes" ]; then
+    memchecklog="$owfn.n.n.x.memcheck.log"
+    do_memcheck "$cmd" "$memchecklog"
+    result=$?
+else
+    echo running $cmd
+    OUTPUT=`$cmd 2>&1`
+    echo $OUTPUT | grep "The two operating guidelines are equal: YES" > /dev/null
+    resultEqual=$?
+    
+    if [ $resultEqual -ne 0 ] ; then
+        echo     ... FAILED
+        echo The two operating guidelines are not equal although they should be.
+        echo
+        result=1
+    fi
+fi
+
+############################################################################
+############################################################################
+
+nodes_mp=1562
+nodes_ann=388
+
+owfn="$testdir/philosophers/phcontrol4.unf.owfn"
+cmd="$FIONA -n $owfn -a -b 4 -t OG"
+
+if [ "$memcheck" = "yes" ]; then
+    memchecklog="$owfn.a.b4.IG.memcheck.log"
+    do_memcheck "$cmd" "$memchecklog"
+    result=$?
+else
+    echo running $cmd
+    OUTPUT=`$cmd 2>&1`
+    echo $OUTPUT | grep "BDD_MP: number of nodes: $nodes_mp" > /dev/null
+    resultNodesMP=$?
+    echo $OUTPUT | grep "BDD_ANN: number of nodes: $nodes_ann" > /dev/null
+    resultNodesANN=$?
+    
+    if [ $resultNodesMP -ne 0 ] ; then
+        echo     ... FAILED
+        echo Number of nodes of BDD_MP is incorrect.
+        echo
+        result=1
+    fi
+
+    if [ $resultNodesANN -ne 0 ] ; then
+        echo     ... FAILED
+        echo Number of nodes of BDD_ANN is incorrect.
+        echo
+        result=1
+    fi
+
+fi
+
+############################################################################
+
+owfn="$testdir/philosophers/phcontrol4.unf.owfn"
+owfn_reference="$testdir/bdd_ref/phcontrol4.unf_reference.owfn"
+cmd="$FIONA -n $owfn -a -b 4 -t OG"
+$cmd 2>/dev/null 1>/dev/null
+
+cmd="$FIONA -n $owfn -n $owfn_reference -x"
+
+
+if [ "$memcheck" = "yes" ]; then
+    memchecklog="$owfn.n.n.x.memcheck.log"
+    do_memcheck "$cmd" "$memchecklog"
+    result=$?
+else
+    echo running $cmd
+    OUTPUT=`$cmd 2>&1`
+    echo $OUTPUT | grep "The two operating guidelines are equal: YES" > /dev/null
+    resultEqual=$?
+    
+    if [ $resultEqual -ne 0 ] ; then
+        echo     ... FAILED
+        echo The two operating guidelines are not equal although they should be.
+        echo
+        result=1
+    fi
+fi
+
+############################################################################
 
 echo
 

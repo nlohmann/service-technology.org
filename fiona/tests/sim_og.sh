8785ce9ad6a19d0eaa241256c7c8f3f013ba4689
danitz <danitz_mail@example.com>
2007-06-24 12:04:08 +0000

    Testsequenz für den Simulations-Check eingefügt.

---
+++ fiona/tests/sim_og.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+############################################################################
+# Copyright 2005 - 2007 Peter Massuthe, Daniela Weinberg, Dennis Reinert,  #
+#                    Jan Bretschneider, Christian Gierds and Robert Danitz #
+#                                                                          #
+# This file is part of Fiona.                                              #
+#                                                                          #
+# Fiona is free software; you can redistribute it and/or modify it         #
+# under the terms of the GNU General Public License as published by the    #
+# Free Software Foundation; either version 2 of the License, or (at your   #
+# option) any later version.                                               #
+#                                                                          #
+# Fiona is distributed in the hope that it will be useful, but WITHOUT     #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for #
+# more details.                                                            #
+#                                                                          #
+# You should have received a copy of the GNU General Public License along  #
+# with Fiona; if not, write to the Free Software Foundation, Inc., 51      #
+# Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.                     #
+############################################################################
+
+echo
+echo ---------------------------------------------------------------------
+echo running $0
+echo
+
+testdir=.
+DIR=$testdir/sim_og
+FIONA=fiona
+
+#loeschen aller erzeugten Dateien im letzten Durchlauf
+rm -f $DIR/*.out
+rm -f $DIR/*.png
+rm -f $DIR/*.og
+rm -f $DIR/*.log
+
+############################################################################
+
+result=0
+
+for i in 1 2 3 4 5 6 8; 
+do
+    #echo running test$i
+
+    owfn1="$DIR/test${i}1.owfn"
+    owfn2="$DIR/test${i}2.owfn"
+
+    OUTPUT=`$FIONA $owfn1 -t OG` 
+    OUTPUT=`$FIONA $owfn2 -t OG` 
+    
+    cmd="$FIONA ${owfn1}.og ${owfn2}.og -t simulation"
+
+    echo running $cmd
+    OUTPUT=`$cmd 2>&1`
+    echo $OUTPUT | grep "The first OG has all the strategies of the second one, possibly more." > /dev/null
+    resultSIM=$?
+    if [ $resultSIM -ne 0 ]
+    then
+        let "result*=1"
+        echo ... Simulation failed, although it should not. 
+    fi
+
+done
+
+for i in 7 9;
+do
+    #echo running test$i
+
+    owfn1="$DIR/test${i}1.owfn"
+    owfn2="$DIR/test${i}2.owfn"
+
+    OUTPUT=`$FIONA $owfn1 -t OG`
+    OUTPUT=`$FIONA $owfn2 -t OG`
+
+    cmd="$FIONA ${owfn1}.og ${owfn2}.og -t simulation"
+
+    echo running $cmd
+    OUTPUT=`$cmd 2>&1`
+    echo $OUTPUT | grep "The second OG has a strategy which the first one hasn't." > /dev/null
+    resultSIM=$?
+    if [ $resultSIM -ne 0 ]
+    then
+        let "result*=1"
+        echo ... Simulation succeded, although it should not.
+    fi
+
+done
+
+echo 
+
+exit $result

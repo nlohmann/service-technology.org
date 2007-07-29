d042257e6cfa55ddd122b1771ddd08a96ae316df
Niels Lohmann <niels.lohmann@gmail.com>
2007-07-29 13:02:13 +0000

    + forgot to check in

---
+++ bpel2owfn/gui/translation-de.ts
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de_DE">
+<defaultcodec></defaultcodec>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui_dialog.h" line="265"/>
+        <source>BPEL2oWFN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="266"/>
+        <source>&amp;About BPEL2oWFN</source>
+        <translation type="unfinished">&amp;Über BPEL2oWFN</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="267"/>
+        <source>Structural reduction</source>
+        <translation type="unfinished">Strukturelle Reduktion</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="268"/>
+        <source>none</source>
+        <translation type="unfinished">keine</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="269"/>
+        <source>Command line</source>
+        <translation type="unfinished">Kommandozeile</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="271"/>
+        <source>use long command-line options</source>
+        <translation type="unfinished">benutze lange Optionsnamen</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="272"/>
+        <source>Output file</source>
+        <translation type="unfinished">Ausgabedatei</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="273"/>
+        <source>Format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="286"/>
+        <source>File name:</source>
+        <translation type="unfinished">Dateiname:</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="275"/>
+        <source>Fiona oWFN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="276"/>
+        <source>LoLA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="277"/>
+        <source>PNML</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="278"/>
+        <source>Graphviz dot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="279"/>
+        <source>Petri net patterns</source>
+        <translation type="unfinished">Petrinetz-Muster</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="280"/>
+        <source>model variables</source>
+        <translation type="unfinished">modelliere Variablen</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="281"/>
+        <source>don&apos;t model faults at all</source>
+        <translation type="unfinished">keine Fehler modellieren</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="282"/>
+        <source>don&apos;t model faults in handlers</source>
+        <translation type="unfinished">keine Fehler in Handlern modellieren</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="283"/>
+        <source>model all faults</source>
+        <translation type="unfinished">alle Fehler modellieren</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="284"/>
+        <source>Translate</source>
+        <translation type="unfinished">Übersetze</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="285"/>
+        <source>Input file</source>
+        <translation type="unfinished">Eingabedatei</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="287"/>
+        <source>Browse</source>
+        <translation type="unfinished">Durchsuchen</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="288"/>
+        <source>Single BPEL process</source>
+        <translation type="unfinished">Einzelner BPEL-Prozess</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="289"/>
+        <source>Translation of a single BPEL process into a Petri net model.</source>
+        <translation type="unfinished">Übersetzung eines einzelnen BPEL-Prozesses in ein Petrinetz-Modell.</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="290"/>
+        <source>BPEL4Chor choreography</source>
+        <translation type="unfinished">BPEL4Chor-Choreographie</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="291"/>
+        <source>Translation of a BPEL4Chor choreography (participant descriptions and participant topology) into a Petri net model.</source>
+        <translation type="unfinished">Übersetzung einer BPEL4Chor-Choreographie (Participant Description und Participant Topology) in eine Petrinetz-Modell.</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="292"/>
+        <source>Static Analysis</source>
+        <translation type="unfinished">Statische Analyse</translation>
+    </message>
+    <message>
+        <location filename="ui_dialog.h" line="293"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">&amp;Hilfe</translation>
+    </message>
+</context>
+</TS>

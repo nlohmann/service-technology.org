574646dd295ef5e5d0fbb1a28bd78cd2de4394a1
Niels Lohmann <niels.lohmann@gmail.com>
2007-07-29 14:04:55 +0000

    + `static_release.sh' script now creates an installation package

--- bpel2owfn/gui/static_release.sh
+++ bpel2owfn/gui/static_release.sh
@@ -3,7 +3,8 @@ export PATH=~/Downloads/qt-static/bin:$PATH
 which qmake
 qmake -config release
 make
-rm -fr release
 mkdir release
 mv BPEL2oWFN.app release
 make distclean
+sudo /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build -proj BPEL2oWFN.pmproj -p BPEL2oWFN.pkg
+rm -fr release

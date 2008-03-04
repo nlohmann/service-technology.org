b007b3db402ca502f775fa8f462758699011bae3
Chris <gierds@informatik.hu-berlin.de>
2008-03-04 13:17:54 +0000

    * everything for adapters

---
+++ fiona/src/adapter.h
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * Copyright 2008 Christian Gierds                                           *
+ *                                                                           *
+ * This file is part of Fiona.                                               *
+ *                                                                           *
+ * Fiona is free software; you can redistribute it and/or modify it          *
+ * under the terms of the GNU General Public License as published by the     *
+ * Free Software Foundation; either version 2 of the License, or (at your    *
+ * option) any later version.                                                *
+ *                                                                           *
+ * Fiona is distributed in the hope that it will be useful, but WITHOUT      *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for  *
+ * more details.                                                             *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License along   *
+ * with Fiona; if not, write to the Free Software Foundation, Inc., 51       *
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.                      *
+ *****************************************************************************/
+
+/*!
+ * \file    adapter.h
+ *
+ * \brief   all functionality for adapter generation
+ *
+ * \author  responsible: Christian Gierds <gierds@informatik.hu-berlin.de>
+ *
+ * \note    This file is part of the tool Fiona and was created during the
+ *          project "Tools4BPEL" at the Humboldt-Universitaet zu Berlin. See
+ *          http://www.informatik.hu-berlin.de/top/tools4bpel for details.
+ *
+ */
+
+#ifndef ADAPTER_H_
+#define ADAPTER_H_
+
+
+#endif /*ADAPTER_H_*/

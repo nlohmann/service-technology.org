eb1a866956bc05d6a1d6fda37aa3f406a4d20056
znamirowski <znamirowski_mail@example.com>
2008-08-20 12:03:26 +0000

    Branch zur neu-Implementation des Frontends und der internen Repräsentation

---
+++ uml2owfnFE/src/globals.cc
@@ -0,0 +1,85 @@
+/*****************************************************************************\
+  GNU BPEL2oWFN -- Translating BPEL Processes into Petri Net Models
+
+  Copyright (C) 2006, 2007  Niels Lohmann,
+                            Christian Gierds, and
+                            Martin Znamirowski
+  Copyright (C) 2005        Niels Lohmann and
+			    Christian Gierds
+
+  GNU BPEL2oWFN is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 3 of the License, or (at your option) any
+  later version.
+
+  GNU BPEL2oWFN is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  GNU BPEL2oWFN (see file COPYING); if not, see http://www.gnu.org/licenses
+  or write to the Free Software Foundation,Inc., 51 Franklin Street, Fifth
+  Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+/*!
+ * \file    globals.cc
+ *
+ * \brief   global varialbes
+ *
+ * \author  Niels Lohmann <nlohmann@informatik.hu-berlin.de>,
+ *          last changes of: \$Author: nielslohmann $
+ * 
+ * \since   2007/03/05
+ *
+ * \date    \$Date: 2007/06/28 07:38:16 $
+ * 
+ * \note    This file is part of the tool BPEL2oWFN and was created during the
+ *          project "Tools4BPEL" at the Humboldt-Universität zu Berlin. See
+ *          http://www.informatik.hu-berlin.de/top/tools4bpel for details.
+ *
+ * \version \$Revision: 1.32 $
+ */
+
+
+
+
+
+/******************************************************************************
+ * Headers
+ *****************************************************************************/
+
+#include "globals.h"
+#include "block.h"
+
+
+
+
+
+namespace globals {
+
+  string finalCondition = "";
+  
+    set<Block*> processes;
+    set<Block*> tasks;
+    set<Block*> services;
+  
+  string program_name ="";
+
+  string invocation = "";
+ 
+  string filename = "<STDIN>";
+  string output_filename = "";
+
+  map<possibleParameters, bool> parameters;
+
+  string last_error_token = "";
+  string last_error_line = "";
+
+  unsigned int reduction_level = 0;
+  
+// <Dirk.F start>
+  string currentProcessName = "";
+// <Dirk.F end>
+}

4f1c0dc1a04f9ca00225f0923613f474a6d160f0
massuthe <massuthe_mail@example.com>
2008-11-07 16:12:45 +0000

    Update of bug [service-tech, bugs #12561] Fiona: phdcontrol3.owfn is controllable with Fiona 2.0, but not with Fiona 3.0:
    + All philosophers now have a FINALMARKING instead of a FINALCONDITION as it is more compact and more intuitive to represent exactly one marking.
    + Synchronized the files in the two different folders.

--- fiona/tests/philosophers.sh
+++ fiona/tests/philosophers.sh
@@ -8,7 +8,7 @@ DIR=$testdir/philosophers
 FIONA=fiona
 
 for i in 3 4 5 6 7 8 9 10; do
-    cmd="$FIONA -t og $DIR/phcontrol$i.unf.owfn -e1 -m1"
+    cmd="$FIONA -t og $DIR/phcontrol$i.unf.owfn -e1"
     if [ "$quiet" != "no" ]; then
         cmd="$cmd -Q"
     fi
@@ -17,9 +17,11 @@ for i in 3 4 5 6 7 8 9 10; do
     echo ---------------------------------------------------------------------
     echo running: $cmd
     $cmd 2>&1
+
     fionaExitCode=$?
     $evaluate $fionaExitCode
-    if [ $? -ne 0 ] 
+
+    if [ $? -ne 0 ]
     then
         result=1
     fi

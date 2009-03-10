8aa50773f5fd35de19df6a3fcc20eff05dbb9996
stephan <stephan_mail@example.com>
2009-03-10 19:39:30 +0000

    * updated test cases in automaton construction (normal & unnormal)

--- pnapi/tests/automaton/test_construction.cc
+++ pnapi/tests/automaton/test_construction.cc
@@ -9,7 +9,7 @@ using namespace pnapi;
 
 int main(int argc, char *argv[])
 {
-  PetriNet net[2];
+  PetriNet net[3];
   ifstream ifstr;
 
   begin_test("Automaton::Automaton() [construction] with normal net");
@@ -17,16 +17,29 @@ int main(int argc, char *argv[])
   try
   {
   ifstr >> io::owfn >> net[0];
+  ifstr.close();
   }
   catch (io::InputError error)
   {
     std::cout << error << "\n";
   }
 //  cout << io::owfn << net[0];
-/*  ifstr.open("sequence_unnormal.owfn", std::ios_base::in);
+  ifstr.open("sequence_unnormal.owfn", std::ios_base::in);
   try
   {
   ifstr >> io::owfn >> net[1];
+  ifstr.close();
+  }
+  catch (io::InputError error)
+  {
+    std::cout << error << "\n";
+  }
+
+/*  ifstr.open("strange_net.owfn", std::ios_base::in);
+  try
+  {
+  ifstr >> io::owfn >> net[2];
+  ifstr.close();
   }
   catch (io::InputError error)
   {
@@ -34,9 +47,11 @@ int main(int argc, char *argv[])
   }*/
   // automaton construction from PetriNet instance
   pnapi::ServiceAutomaton sauto0(net[0]);
-//  pnapi::ServiceAutomaton sauto1(net[1]);
+  pnapi::ServiceAutomaton sauto1(net[1]);
+//  pnapi::ServiceAutomaton sauto2(net[2]);
   cout << io::sa << sauto0;
-//  cout << io::sa << sauto1;
+  cout << io::sa << sauto1;
+//  cout << io::sa << sauto2;
   end_test();
 
   return 0;

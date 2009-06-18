74630ca62418c8b805e84c4d3f8a8c0ec1441c87
Chris <gierds@informatik.hu-berlin.de>
2009-06-18 13:06:38 +0000

    This was a nasty one, has anyone ever tested this in detail!? The test_construction binary ignored every error and just continued (that's the reason why there was no failure when calling make check!)
    * test files have to begin with test_construction. , otherwise the test wrapper will not copy them to the temp dir and the binary will always fail to find the files
    * small changes to Makefile.am in order to use configured_*FLAGS

--- pnapi/tests/automaton/test_construction.cc
+++ pnapi/tests/automaton/test_construction.cc
@@ -3,6 +3,7 @@
 #include "../../src/io.h"
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 using std::ifstream;
 using std::fstream;
@@ -10,49 +11,58 @@ using std::cout;
 using std::endl;
 using namespace pnapi;
 
+
 int main(int argc, char *argv[])
 {
   PetriNet net[4];
-  ifstream ifstr;
-
+  
   begin_test("Automaton::Automaton() [construction] with normal net");
-
-  ifstr.open("sequence_normal.net.owfn", std::ios_base::in);
+  
+  ifstream ifstr;
+  ifstr.open("test_construction.sequence_normal_net.owfn");
   try
   {
-  ifstr >> io::owfn >> net[0];
+    ifstr >> io::owfn >> net[0];
   }
   catch (io::InputError error)
   {
-    std::cout << error << "\n";
+    std::cerr << error << "\n";
+    ifstr.close();
+    exit(EXIT_FAILURE);
   }
   ifstr.close();
   Automaton sauto0(net[0]);
   cout << io::sa << sauto0;
 
-  ifstr.open("sequence_unnormal.net.owfn", std::ios_base::in);
+  ifstr.clear();
+  ifstr.open("test_construction.sequence_unnormal_net.owfn", std::ios_base::in);
   try
   {
-  ifstr >> io::owfn >> net[1];
+    ifstr >> io::owfn >> net[1];
   }
   catch (io::InputError error)
   {
     std::cout << error << "\n";
+    ifstr.close();
+    exit(EXIT_FAILURE);
   }
   ifstr.close();
   Automaton sauto1(net[1]);
   cout << io::sa << sauto1;
 
-  ifstr.open("strange_net.net.owfn", std::ios_base::in);
+  ifstr.clear();
+  ifstr.open("test_construction.strange_net.owfn", std::ios_base::in);
   try
   {
-  ifstr >> io::owfn >> net[2];
-  ifstr.close();
+    ifstr >> io::owfn >> net[2];
   }
   catch (io::InputError error)
   {
-    std::cout << error << "\n";
+    std::cerr << error << "\n";
+    ifstr.close();
+    exit(EXIT_FAILURE);
   }
+  ifstr.close();
   pnapi::Automaton sauto2(net[2]);
   cout << io::sa << sauto2;
 

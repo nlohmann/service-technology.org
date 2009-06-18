63ed08e5f6eee1826a9b40d12c8343caba30dbef
Chris <gierds@informatik.hu-berlin.de>
2009-06-18 11:31:00 +0000

    * test test_produce crashed under cygwin: reopening ifstream ifs for the constraint owfn resulted in the filepointer being at the streams end (don't ask why) * add try/catch block in order to catch parse errors

--- pnapi/tests/petrinet/test_produce.cc
+++ pnapi/tests/petrinet/test_produce.cc
@@ -2,6 +2,8 @@
 #include <map>
 #include "test.h"
 
+#define CHECK std::cerr << "check " << __FILE__ << "@" << __LINE__ << std::endl;
+
 using std::map;
 using std::ofstream;
 using std::ifstream;
@@ -14,13 +16,29 @@ int main(int argc, char * argv[])
 
   PetriNet net;
   ifs.open("test_produce.onlineshop.owfn");
-  ifs >> owfn >> net;
+  try {
+    ifs >> owfn >> net;
+  }
+  catch (pnapi::io::InputError error) {
+    std::cerr << "test_produce:" << error << endl;
+    ifs.close();
+    exit(EXIT_FAILURE);
+  }
   ifs.close();
  
+  ifstream ifs2;
   PetriNet constraint;
-  ifs.open("test_produce.onlineshop.constraint.owfn");
-  ifs >> owfn >> constraint;
-  ifs.close();
+  ifs2.open("test_produce.onlineshop.constraint.owfn");
+  try {
+    ifs2 >> owfn >> constraint;
+  }
+  catch (pnapi::io::InputError error) 
+  {
+    std::cerr << "test_produce:" << error << endl;
+    ifs2.close();
+    exit(EXIT_FAILURE);
+  }
+  ifs2.close();
 
   begin_test("PetriNet::produce() [product with constraint oWFN]");
   net.produce(constraint);

e045c3658a666aa2b2bf782f0bf51cb77d9e6292
cas <cas_mail@example.com>
2009-06-24 18:35:56 +0000

    * added script for public view computation (owfn -> owfn)

---
+++ sayo/src/publicview.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+NET=$1
+WENDY=wendy
+SA2SM=sa2sm
+SAYO=sayo
+
+${WENDY} --og=${NET}.og --formula=2bits ${NET} &> /dev/null
+
+${SAYO} -i ${NET}.og -o ${NET}.sa
+
+${SA2SM} ${NET}.sa ${NET}.pv.owfn
+

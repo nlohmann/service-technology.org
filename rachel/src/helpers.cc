6ac67bfe54e6aff53d5afaa5f05e42de904ea445
Niels Lohmann <niels.lohmann@gmail.com>
2009-10-20 21:52:27 +0000

    * updated copyright header to AGPL

--- rachel/src/helpers.cc
+++ rachel/src/helpers.cc
@@ -1,31 +1,25 @@
 /*****************************************************************************\
- Rachel -- Repairing Automata for Choreographies by Editing Labels
- 
- Copyright (C) 2008, 2009  Niels Lohmann <niels.lohmann@uni-rostock.de>
- 
- Rachel is free software; you can redistribute it and/or modify it under the
- terms of the GNU General Public License as published by the Free Software
- Foundation; either version 3 of the License, or (at your option) any later
- version.
- 
+ Rachel -- Reparing Service Choreographies
+
+ Copyright (c) 2008, 2009 Niels Lohmann
+
+ Rachel is free software: you can redistribute it and/or modify it under the
+ terms of the GNU Affero General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option)
+ any later version.
+
  Rachel is distributed in the hope that it will be useful, but WITHOUT ANY
- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License along with
- Rachel (see file COPYING); if not, see http://www.gnu.org/licenses or write to
- the Free Software Foundation,Inc., 51 Franklin Street, Fifth
- Floor, Boston, MA 02110-1301  USA.
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+ more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Rachel.  If not, see <http://www.gnu.org/licenses/>.
 \*****************************************************************************/
 
 #include <config.h>
-#include <string>
-#include <map>
-#include <sstream>
 #include "Graph.h"
 #include "helpers.h"
-
-using std::map;
 
 
 /*!

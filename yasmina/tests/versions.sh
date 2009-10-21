86c406bd35a1d10af65291b597ad740ac33d4f19
Niels Lohmann <niels.lohmann@gmail.com>
2009-10-21 17:00:32 +0000

    * tidied autotools environment -- cover could/should work now * lcov/zcov will be added again

--- yasmina/tests/versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for tool in autoconf autom4te automake bash bison dot flex gcc gmake gengetopt help2man libtool make makeinfo petri petrify4.2 petrify4.1 petrify4.0 petrify sh; do
-    echo "$tool (`which $tool || echo "not found"`)"
-    echo "------------------------------------------------------------------------"
-    echo `($tool --version &> /dev/null && $tool --version) || ($tool -V &> /dev/null && $tool -V)  || ($tool -v &> /dev/null && $tool -v) || echo "$tool was not found"` | fmt
-    echo
-done

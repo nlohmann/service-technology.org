c47582a3d66c902de55acfdbe23da7d0665d1b3a
cas <cas_mail@example.com>
2009-11-17 10:55:43 +0000

    * solved task #6811: PNAPI: Scripts to update API

--- meta/scripts/get-pnapi.sh
+++ meta/scripts/get-pnapi.sh
@@ -8,6 +8,27 @@
 # IMPORTANT: Call this script from the customer tool's root directory! #
 ########################################################################
 
+# print information
+echo
+echo "  This script will get the most recent version of the"
+echo "  Petri Net API from 'svn://svn.gna.org/service-tech/trunk/pnapi'"
+echo "  and will install it in the relative directory './libs/pnapi/'."
+echo
+echo "  If a previous version of the PNAPI is alreadey installed,"
+echo "  it will be removed before installing the newer one."
+echo "  If not, all needed directories will be created."
+echo
+echo "  This script WILL NOT touch any subversion information ('.svn')"
+echo "  in the directory './libs/pnapi'."
+echo "  You have to take care of this by yourself."
+echo
+echo
+
+# wait for key pressed
+read -n 1 -p "    Press any key to continue"
+echo
+
+
 # create "libs/pnapi" directory if it does not exists
 mkdir -p ./libs/pnapi
 
@@ -26,3 +47,10 @@ rm -rf ./libs/pnapi-tmp
 
 # replace original Makefile.am by the one designed for customer tools
 mv -f ./libs/pnapi/Makefile.am.customer ./libs/pnapi/Makefile.am
+
+# tell user we have finished
+echo
+echo "  Finished installing PNAPI"
+echo "  Please check, whether all files are versioned properly in your tool."
+echo
+

7158037fc46b4ac57d4ab0217fc952a01e3aec5d
Niels Lohmann <niels.lohmann@gmail.com>
2009-11-22 15:10:48 +0000

    * added copyright header

--- wendy/src/FullMarkingQueue.cc
+++ wendy/src/FullMarkingQueue.cc
@@ -1,11 +1,33 @@
+/*****************************************************************************\
+ Wendy -- Synthesizing Partners for Services
+
+ Copyright (c) 2009 Niels Lohmann, Christian Sura, and Daniela Weinberg
+
+ Wendy is free software: you can redistribute it and/or modify it under the
+ terms of the GNU Affero General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option)
+ any later version.
+
+ Wendy is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+ more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Wendy.  If not, see <http://www.gnu.org/licenses/>.
+\*****************************************************************************/
+
+
 #include <config.h>
 #include <cstdlib>
 #include "FullMarkingQueue.h"
+
 
 size_t FullMarkingQueue::maximal_length = 0;
 size_t FullMarkingQueue::initial_length = 128;
 size_t FullMarkingQueue::current_objects = 0;
 size_t FullMarkingQueue::maximal_objects = 0;
+
 
 FullMarkingQueue::FullMarkingQueue()
   : storage(static_cast<FullMarking**>(malloc(initial_length * SIZEOF_VOIDP))),

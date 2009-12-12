db30fcf2ae2f18a60d35704eca9088c6c061454b
Niels Lohmann <niels.lohmann@gmail.com>
2009-12-12 20:39:19 +0000

    * canonified configure script and Makefiles * using Output class for output * added --bug parameter * small refactoring to main.cc * updated verbose.* * small adjustments to the output messages

--- rebecca/src/main.cc
+++ rebecca/src/main.cc
@@ -1,7 +1,7 @@
+#include <config.h>
 #include <climits>
 #include <cstdlib>
-#include <fstream>
-#include <sstream>
+#include <libgen.h>
 #include <string>
 #include <vector>
 #include "choreography.h"
@@ -9,11 +9,15 @@
 #include "rebeccaio.h"
 #include "peerautomaton.h"
 #include "verbose.h"
+#include "config-log.h"
+#include "Output.h"
 
-#include <iostream>
 
 /// the command line parameters
 gengetopt_args_info args_info;
+
+/// the invocation string
+std::string invocation;
 
 using std::string;
 using std::vector;
@@ -24,10 +28,16 @@ Choreography * chor;
 extern int chor_parse();
 extern FILE *chor_in;
 
+
 /// evaluate the command line parameters
 void evaluateParameters(int argc, char** argv) {
-    // set default values
-    //argv[0] = (char *)PACKAGE;
+    // overwrite invocation for consistent error messages
+    argv[0] = basename(argv[0]);
+
+    // store invocation in a std::string for meta information in file output
+    for (int i = 0; i < argc; ++i) {
+        invocation += std::string(argv[i]) + " ";
+    }
 
     cmdline_parser_init(&args_info);
 
@@ -37,11 +47,22 @@ void evaluateParameters(int argc, char** argv) {
     // call the cmdline parser
     cmdline_parser(argc, argv, &args_info);
 
+    // debug option
+    if (args_info.bug_flag) {
+        { Output debug_output("bug.log", "configuration information");
+        debug_output.stream() << CONFIG_LOG; }
+        message("Please send file '%s' to %s!", _cfilename_("bug.log"), _coutput_(PACKAGE_BUGREPORT));
+        exit(EXIT_SUCCESS);
+    }
+
     free(params);
 }
 
 
 int main(int argc, char** argv) {
+    // set a standard filename
+    std::string filename = std::string(PACKAGE) + "_output";
+
     /*--------------------------------------.
     | 0. parse the command line parameters  |
     `--------------------------------------*/
@@ -50,11 +71,11 @@ int main(int argc, char** argv) {
     /*--------------------------------------.
     | 1. parse the chreography description  |
     `--------------------------------------*/
-    if (args_info.inputs_num > 0)
-    {
-      chor_in = fopen(args_info.inputs[0], "r");
-      if (!chor_in)
-        abort(1, "could not open the input file '%s'", args_info.inputs[0]);
+    if (args_info.inputs_num > 0) {
+        chor_in = fopen(args_info.inputs[0], "r");
+        if (!chor_in) {
+            abort(1, "could not open the input file '%s'", args_info.inputs[0]);
+        }
     }
     chor = new Choreography();
     status("parsing input choreography");
@@ -66,47 +87,45 @@ int main(int argc, char** argv) {
     `----------------------------*/
     // * check whether choreography is sane (each input has an output)
     status("checking whether input model is a choreography");
-    if (!chor->isChoreography())
-      abort(10, "choreography definition violated");
-    status("input model is a choreography");
+    if (not chor->isChoreography()) {
+        abort(10, "choreography definition violated");
+    } else {
+        status("input model is a choreography");        
+    }
+
     // * create an array to quickly check whether two events are distant
     status("creating an array to quickly check whether two events are distant");
-    bool ** distantMessages = chor->distantEvents();
+    bool** distantMessages = chor->distantEvents();
 
     std::vector<PeerAutomaton *> projected;
-    for (int i = 0; i < (int) chor->collaboration().size(); ++i)
-    {
-      PeerAutomaton * at = new PeerAutomaton();
-      for (int a = 0; a < (int) chor->events().size(); ++a)
-      {
-        if (chor->collaboration()[i]->input().count(chor->events()[a]))
-        {
-          for (std::set<Edge *>::iterator e = chor->edges().begin();
-              e != chor->edges().end(); ++e)
-            if ((*e)->label == chor->events()[a])
-            {
-              if ((*e)->type == SYN)
-                at->pushSynchronous(chor->events()[a]);
-              else
-                at->pushInput(chor->events()[a]);
-              break;
+    for (size_t i = 0; i < chor->collaboration().size(); ++i) {
+        PeerAutomaton * at = new PeerAutomaton();
+        for (size_t a = 0; a < chor->events().size(); ++a) {
+            if (chor->collaboration()[i]->input().count(chor->events()[a])) {
+                for (std::set<Edge *>::iterator e = chor->edges().begin(); e != chor->edges().end(); ++e)
+                if ((*e)->label == chor->events()[a]) {
+                    if ((*e)->type == SYN) {
+                        at->pushSynchronous(chor->events()[a]);
+                    } else {
+                        at->pushInput(chor->events()[a]);
+                    }
+                    break;
+                }
+            }
+            if (chor->collaboration()[i]->output().count(chor->events()[a])) {
+                for (std::set<Edge *>::iterator e = chor->edges().begin(); e != chor->edges().end(); ++e) {
+                    if ((*e)->label == chor->events()[a]) {
+                        if ((*e)->type == SYN) {
+                            at->pushSynchronous(chor->events()[a]);
+                        } else {
+                            at->pushOutput(chor->events()[a]);
+                        }
+                        break;
+                    }
+                }
             }
         }
-        if (chor->collaboration()[i]->output().count(chor->events()[a]))
-        {
-          for (std::set<Edge *>::iterator e = chor->edges().begin();
-              e != chor->edges().end(); ++e)
-            if ((*e)->label == chor->events()[a])
-            {
-              if ((*e)->type == SYN)
-                at->pushSynchronous(chor->events()[a]);
-              else
-                at->pushOutput(chor->events()[a]);
-              break;
-            }
-        }
-      }
-      projected.push_back(at);
+        projected.push_back(at);
     }
 
     bool complete = true;
@@ -117,175 +136,163 @@ int main(int argc, char** argv) {
     | 3. main algorithm: resolution of dependency  |
     `---------------------------------------------*/
     bool changed = true;
-    while (changed)
-    {
-      changed = false;
-      /// 3.1 if a disables b
-      for (std::set<int>::iterator q = chor->states().begin();
-          q != chor->states().end(); ++q)
-        for (int a = 0; a < (int) chor->events().size(); ++a)
-          for (int b = 0; b < (int) chor->events().size(); ++b)
-          {
-            if (!distantMessages[a][b])
-              continue;
-            if (chor->disables(*q, a, b))
-            {
-              status("%s disables %s in state %d", chor->events()[a].c_str(),
-                  chor->events()[b].c_str(), *q);
-              complete = false;
-              changed = true;
-              int qa = chor->createState();
-              int qb = chor->createState();
-              std::set<Edge *> qE = chor->edgesFrom(*q);
-              for (std::set<Edge *>::iterator e = qE.begin(); e != qE.end();
-                  ++e)
-              {
-                if ((*e)->label != chor->events()[a])
-                  chor->createEdge(qa, (*e)->label, (*e)->destination,
-                      (*e)->type);
-                if ((*e)->label != chor->events()[b])
-                  chor->createEdge(qb, (*e)->label, (*e)->destination,
-                      (*e)->type);
-                chor->deleteEdge(*e);
-              }
-              chor->createEdge(*q, "", qa, CHI);
-              chor->createEdge(*q, "", qb, CHI);
+    while (changed) {
+        changed = false;
+          /// 3.1 if a disables b
+        for (std::set<int>::iterator q = chor->states().begin(); q != chor->states().end(); ++q) {
+            for (size_t a = 0; a < chor->events().size(); ++a) {
+                for (size_t b = 0; b < chor->events().size(); ++b) {
+                    if (!distantMessages[a][b]) {
+                        continue;
+                    }
+                    if (chor->disables(*q, a, b)) {
+                        status("event %s disables event %s in state %d", chor->events()[a].c_str(),
+                            chor->events()[b].c_str(), *q);
+                        complete = false;
+                        changed = true;
+                        int qa = chor->createState();
+                        int qb = chor->createState();
+                        std::set<Edge *> qE = chor->edgesFrom(*q);
+                        for (std::set<Edge *>::iterator e = qE.begin(); e != qE.end(); ++e) {
+                            if ((*e)->label != chor->events()[a]) {
+                                chor->createEdge(qa, (*e)->label, (*e)->destination, (*e)->type);
+                            }
+                            if ((*e)->label != chor->events()[b]) {
+                                chor->createEdge(qb, (*e)->label, (*e)->destination, (*e)->type);
+                            }
+                            chor->deleteEdge(*e);
+                        }
+                        chor->createEdge(*q, "", qa, CHI);
+                        chor->createEdge(*q, "", qb, CHI);
+                    }
+                }
             }
-          }
-      /// 3.2 if a enables b
-      for (std::set<int>::iterator q = chor->states().begin();
-          q != chor->states().end(); ++q)
-        for (int a = 0; a < (int) chor->events().size(); ++a)
-          for (int b = 0; b < (int) chor->events().size(); ++b)
-          {
-            if (!distantMessages[a][b])
-              continue;
-            if (chor->enables(*q, a, b))
-            {
-              status("%s enables %s in state %d", chor->events()[a].c_str(),
-                  chor->events()[b].c_str(), *q);
-              complete = distributed = false;
-              changed = true;
-              int qab = chor->findState(*q, a, b);
-              chor->deleteState(qab);
+        }
+          /// 3.2 if a enables b
+        for (std::set<int>::iterator q = chor->states().begin(); q != chor->states().end(); ++q) {
+            for (size_t a = 0; a < chor->events().size(); ++a) {
+                for (size_t b = 0; b < chor->events().size(); ++b) {
+                    if (!distantMessages[a][b]) {
+                        continue;
+                    }
+                    if (chor->enables(*q, a, b)) {
+                        status("event %s enables event %s in state %d", chor->events()[a].c_str(), chor->events()[b].c_str(), *q);
+                        complete = distributed = false;
+                        changed = true;
+                        int qab = chor->findState(*q, a, b);
+                        chor->deleteState(qab);
+                    }
+                }
             }
-          }
-      /// 3.3 if q_ab and q_ba are not equivalent
-      for (std::set<int>::iterator q = chor->states().begin();
-          q != chor->states().end(); ++q)
-        for (int a = 0; a < (int) chor->events().size(); ++a)
-          for (int b = 0; b < (int) chor->events().size(); ++b)
-          {
-            if (!distantMessages[a][b])
-              continue;
-            int qab = chor->findState(*q, a, b);
-            int qba = chor->findState(*q, b, a);
-            if (qab != -1 && qba != -1 && !chor->equivalent(qab, qba))
-            {
-              status("States %d and %d are not equivalent", qab, qba);
-              complete = distributed = false;
-              changed = true;
-              chor->unite(qab, qba);
+        }
+          /// 3.3 if q_ab and q_ba are not equivalent
+        for (std::set<int>::iterator q = chor->states().begin(); q != chor->states().end(); ++q) {
+            for (size_t a = 0; a < chor->events().size(); ++a) {
+                for (size_t b = 0; b < chor->events().size(); ++b) {
+                    if (!distantMessages[a][b]) {
+                        continue;
+                    }
+                    int qab = chor->findState(*q, a, b);
+                    int qba = chor->findState(*q, b, a);
+                    if (qab != -1 && qba != -1 && !chor->equivalent(qab, qba)) {
+                        status("states %d and %d are not equivalent", qab, qba);
+                        complete = distributed = false;
+                        changed = true;
+                        chor->unite(qab, qba);
+                    }
+                }
             }
-          }
+        }
     }
 
-    if (args_info.Chor_given)
-    {
-      std::cout << *chor;
-    }
-    if (args_info.chor_given)
-    {
-      std::ofstream chorFile(args_info.chor_arg);
-      chorFile << *chor;
-      chorFile.close();
+    if (chor->initialState() == INT_MIN) {
+        partial = false;
     }
 
-    if (chor->initialState() == INT_MIN)
-      partial = false;
-    std::cerr << "The input choreography is" << std::endl
-              << "  COMPLETELY REALIZABLE:    " << (complete ? "YES" : "NO") << std::endl
-              << "  DISTRIBUTEDLY REALIZABLE: " << (distributed ? "YES" : "NO") << std::endl
-              << "  PARTIALLY REALIZABLE:     " << (partial ? "YES" : "NO") << std::endl;
+    // display analysis result
+    message("choreography is completely realizable:    %s",
+        (complete ? _cgood_("YES") : _cbad_("NO")));
+    message("choreography is distributedly realizable: %s",
+        (distributed ? _cgood_("YES") : _cbad_("NO")));
+    message("choreography is partially realizable:     %s",
+        (partial ? _cgood_("YES") : _cbad_("NO")));
+
+    // output resolved choreography if requested
+    if (args_info.chor_given) {
+        std::string chor_filename = args_info.chor_arg ? args_info.chor_arg : filename + ".chor";
+        Output output(chor_filename, "choreography");
+        output.stream() << *chor;
+    }
 
     /*--------------------------------.
     | 4. final step: peer projection  |
     `--------------------------------*/
-    for (int i = 0; i < (int) chor->collaboration().size(); ++i)
-    {
-      PeerAutomaton * a = projected[i];
-      /*
-       * 4.1 calculate q0
-       */
-      std::set<int> q0;
-      q0.insert(chor->initialState());
-      q0 = chor->closure(q0, i);
-      a->pushState(q0);
-      a->setInitialState(q0);
-      if (chor->isFinal(q0))
-        a->pushFinalState(q0);
-      /*
-       * 4.2 calculate all other states
-       */
-      for (std::set<std::set<int> >::iterator q = a->states().begin();
-          q != a->states().end(); ++q)
-      {
-        std::set<Edge *> E = chor->edgesFrom(*q);
-        for (std::set<Edge *>::iterator e = E.begin(); e != E.end(); ++e)
-        {
-          std::set<int> qprime;
-          if (a->isState((*e)->destination))
-          {
-            qprime = a->findState((*e)->destination);
-            if (qprime == *q)
-              continue;
-          }
-          else
-          {
-            qprime.insert((*e)->destination);
-            qprime = chor->closure(qprime, i);
-            a->pushState(qprime);
-          }
-          a->createEdge(*q, (*e)->label, qprime, (*e)->type);
-          if (chor->isFinal(qprime))
-            a->pushFinalState(qprime);
+    for (size_t i = 0; i < chor->collaboration().size(); ++i) {
+        PeerAutomaton * a = projected[i];
+        /*
+         * 4.1 calculate q0
+         */
+        std::set<int> q0;
+        q0.insert(chor->initialState());
+        q0 = chor->closure(q0, i);
+        a->pushState(q0);
+        a->setInitialState(q0);
+        if (chor->isFinal(q0)) {
+            a->pushFinalState(q0);
         }
-      }
+        /*
+         * 4.2 calculate all other states
+         */
+        for (std::set<std::set<int> >::iterator q = a->states().begin();  q != a->states().end(); ++q) {
+            std::set<Edge *> E = chor->edgesFrom(*q);
+            for (std::set<Edge *>::iterator e = E.begin(); e != E.end(); ++e) {
+                std::set<int> qprime;
+                if (a->isState((*e)->destination)) {
+                    qprime = a->findState((*e)->destination);
+                    if (qprime == *q) {
+                        continue;
+                    }
+                } else {
+                    qprime.insert((*e)->destination);
+                    qprime = chor->closure(qprime, i);
+                    a->pushState(qprime);
+                }
+                a->createEdge(*q, (*e)->label, qprime, (*e)->type);
+                if (chor->isFinal(qprime)) {
+                    a->pushFinalState(qprime);
+                }
+            }
+        }
     }
 
     /*----------------.
     | 5. file output  |
     `----------------*/
-    if (!args_info.quiet_flag)
-    {
-      // set file prefix
-      std::string fprefix = "";
-      if (args_info.prefix_given)
-      {
-        fprefix = args_info.prefix_arg;
-        fprefix.push_back('.');
-      }
-      std::ofstream file;
-      for (int i = 0; i < (int) projected.size(); ++i)
-      {
-        std::string filename =
-            fprefix+"peer"+chor->collaboration()[i]->name()+".sa";
-        file.open(filename.c_str());
-        file << *projected[i];
-        file.close();
-      }
+    if (!args_info.quiet_flag) {
+        // set file prefix
+        std::string fprefix = "";
+        if (args_info.prefix_given) {
+            fprefix = args_info.prefix_arg;
+            fprefix.push_back('.');
+        }
+        for (size_t i = 0; i < projected.size(); ++i) {
+            std::string peer_filename(fprefix + "peer" + chor->collaboration()[i]->name() + ".sa");
+            Output output(peer_filename, "peer automaton " + chor->collaboration()[i]->name());
+            output.stream() << *projected[i];
+        }
     }
 
     /*-------------.
     | 6. Clean up  |
     `--------------*/
-    for (int i = 0; i < (int) chor->events().size(); ++i)
-      delete [] distantMessages[i];
+    for (size_t i = 0; i < chor->events().size(); ++i) {
+        delete [] distantMessages[i];
+    }
     delete [] distantMessages;
     delete chor;
-    for (int i = 0; i < (int) projected.size(); ++i)
-      delete projected[i];
-    projected.clear();
+    for (size_t i = 0; i < projected.size(); ++i) {
+        delete projected[i];
+    }
 
     return EXIT_SUCCESS;
 }
--- rebecca/src/verbose.cc
+++ rebecca/src/verbose.cc
@@ -1,65 +1,159 @@
 /*****************************************************************************\
- Mia -- calculating migration information
+ Wendy -- Synthesizing Partners for Services
 
- Copyright (C) 2009  Niels Lohmann <niels.lohmann@uni-rostock.de>
+ Copyright (c) 2009 Niels Lohmann, Christian Sura, and Daniela Weinberg
 
- Mia is free software: you can redistribute it and/or modify it under the
+ Wendy is free software: you can redistribute it and/or modify it under the
  terms of the GNU Affero General Public License as published by the Free
  Software Foundation, either version 3 of the License, or (at your option)
  any later version.
 
- Mia is distributed in the hope that it will be useful, but WITHOUT ANY
+ Wendy is distributed in the hope that it will be useful, but WITHOUT ANY
  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
  more details.
 
  You should have received a copy of the GNU Affero General Public License
- along with Mia.  If not, see <http://www.gnu.org/licenses/>. 
+ along with Wendy.  If not, see <http://www.gnu.org/licenses/>. 
 \*****************************************************************************/
+
 
 #include <cstdarg>
 #include <cstdlib>
-
-#include "config.h"
+#include <cerrno>
+#include <cstring>
+#include <string>
+#include <fstream>
 #include "cmdline.h"
+#include "verbose.h"
 
 extern gengetopt_args_info args_info;
 
-void status(const char* format, ...) {
-    if (args_info.verbose_flag == 0) {
-        return;
-    }
 
-    fprintf(stderr, "%s: ", PACKAGE);
+/***************************************************************************\
+ * syslog functionalities (to be enabled with "configure --enable-syslog") *
+\***************************************************************************/
+#ifdef USE_SYSLOG
+#include <syslog.h>
+
+extern std::string invocation;
+
+/// dummy class to have global constructor and destructor
+namespace st {
+    class Logger {
+        public:
+            Logger() {
+                openlog(PACKAGE, LOG_PID, LOG_USER);
+                syslog(LOG_NOTICE, "--> starting %s", PACKAGE_STRING);
+            }
+
+            ~Logger() {
+                syslog(LOG_NOTICE, "<-- done: %s", invocation.c_str());
+                closelog();
+            }
+    };
+
+    /// dummy object living in global namespace
+    Logger myLogger;
+}
+#endif
+
+
+/*!
+ \param format  the status message formatted as printf string
+ 
+ \note use this function rather sparsely in order not to spam the output
+*/
+void message(const char* format, ...) {
+    fprintf(stderr, "%s: ", _ctool_(PACKAGE));
 
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
-    va_end (args);
+    va_end(args);
 
-    fprintf(stderr, "\n");    
+#ifdef USE_SYSLOG
+    va_start(args, format);
+    vsyslog(LOG_NOTICE, format, args);
+    va_end(args);
+#endif
+
+    fprintf(stderr, "\n");
 }
 
 
 /*!
- \brief abort with an error message and an error code
- 
- The codes are documented in Wendy's manual.
- 
- \param code    the error code
- \param format  the error message formatted as printf string
+ \param format  the status message formatted as printf string
 */
-void abort(unsigned int code, const char* format, ...) {
-    fprintf(stderr, "%s: ", PACKAGE);
+void status(const char* format, ...) {
+    if (not args_info.verbose_flag) {
+        return;
+    }
+
+    fprintf(stderr, "%s: ", _ctool_(PACKAGE));
 
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
-    va_end (args);
+    va_end(args);
 
-    fprintf(stderr, " -- aborting [#%02d]\n", code);
+#ifdef USE_SYSLOG
+    va_start(args, format);
+    vsyslog(LOG_NOTICE, format, args);
+    va_end(args);
+#endif
 
+    fprintf(stderr, "\n");
+}
+
+
+/*!
+ \param code    the error code
+ \param format  the error message formatted as printf string
+
+ \note The codes should be documented in the manual.
+*/
+void abort(unsigned short code, const char* format, ...) {
+    fprintf(stderr, "%s: %s", _ctool_(PACKAGE), _c0_);
+
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+
+#ifdef USE_SYSLOG
+    va_start(args, format);
+    vsyslog(LOG_ERR, format, args);
+    va_end(args);
+#endif
+
+    fprintf(stderr, "%s -- %saborting [#%02d]%s\n", _c_, _cR_, code, _c_);
     status("see manual for a documentation of this error");
 
+    if (errno != 0) {
+        status("last error message: %s", strerror(errno));
+    }
+
     exit(EXIT_FAILURE);
+}
+
+
+
+void displayFileError(char* filename, int lineno, char* token) {
+    std::ifstream f(filename);
+    std::string line;
+    for (int i = 0; i < lineno; ++i) {
+        getline(f, line);
+    }
+    size_t firstpos(line.find_first_of(token));
+    std::string format = line.replace(firstpos, strlen(token), std::string(_cbad_(token)));
+    fprintf(stderr, "  %s\n", line.c_str());
+    fprintf(stderr, "  ");
+    for (unsigned int i = 0; i < firstpos; ++i) {
+        fprintf(stderr, "  ");
+    }
+    for (unsigned int i = 0; i < strlen(token); ++i) {
+        fprintf(stderr, "^");
+    }
+    fprintf(stderr, "\n");
 }

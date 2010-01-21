e3855b8ac7eb965708d1eae66b1932fb162b54fa
Richard Müller <github@richardmueller.net>
2010-01-21 13:53:17 +0000

    - introduced Output class and verbose methods from wendy (see task #6883 https://gna.org/task/?6883) - enabled compact make output

--- linda/src/main.cc
+++ linda/src/main.cc
@@ -13,6 +13,17 @@
 #include "stateEquation.h"
 #include "files.h"
 #include "eventTermParser.h"
+#include "Output.h"
+#include "verbose.h"
+
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <libgen.h>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 /// the command line parameters
 gengetopt_args_info args_info;
@@ -55,6 +66,133 @@ extern int etc_yyparse();
 #ifdef YY_FLEX_HAS_YYLEX_DESTROY
 extern int etc_yylex_destroy();
 #endif
+
+
+/// Output a message profile.
+void output(std::ostream& file, ExtendedStateEquation** systems, pnapi::PetriNet* net, uint8_t nr) {
+
+	file << "PLACE";
+	file << std::endl << "  INTERNAL" << std::endl << "    ";
+
+	bool first = true;
+
+	for (std::set<pnapi::Place *>::iterator it =
+			net->getInternalPlaces().begin(); it
+			!= net->getInternalPlaces().end(); ++it) {
+
+		if (first) {
+			first = false;
+		} else {
+			file << ",";
+		}
+
+		file << (*it)->getName();
+
+	}
+
+	file << ";";
+
+	file << std::endl << "  INPUT";
+
+	first = true;
+
+	for (std::set<pnapi::Place *>::iterator it = net->getInputPlaces().begin(); it
+			!= net->getInputPlaces().end(); ++it) {
+
+		if (first) {
+			first = false;
+		} else {
+			file << ",";
+		}
+
+		file << std::endl << "    ";
+
+		file << (*it)->getName();
+
+	}
+
+	file << ";";
+	file << std::endl << "  OUTPUT";
+
+	first = true;
+
+	for (std::set<pnapi::Place *>::iterator it = net->getOutputPlaces().begin(); it
+			!= net->getOutputPlaces().end(); ++it) {
+
+		if (first) {
+			first = false;
+		} else {
+			file << ",";
+		}
+
+		file << std::endl << "    ";
+
+		file << (*it)->getName();
+
+	}
+
+	file << ";";
+
+	file << std::endl << std::endl << "FINALMARKINGS";
+
+	for (int i = 0; i < nr; ++i) {
+		file << std::endl << "    F" << i+1 << ": "
+				<< systems[i]->omega->toString();
+	}
+
+	int counter = 1;
+
+	file << std::endl << std::endl << "TERMS";
+
+	for (std::vector<int*>::iterator termsIt =
+			systems[0]->calculatedEventTerms.begin(); termsIt
+			!= systems[0]->calculatedEventTerms.end(); ++termsIt) {
+		file << std::endl << "    T" << counter << ": ";
+		for (int i = 0; i < LindaHelpers::NR_OF_EVENTS; ++i) {
+			if ((*termsIt)[i] == 0) {
+				continue;
+			}
+
+			if ((*termsIt)[i] > 0) {
+				file << "+";
+			}
+			file << (*termsIt)[i] << "*" << LindaHelpers::EVENT_STRINGS[i] << " ";
+		}
+		file << ";";
+		++counter;
+
+	}
+
+	file << std::endl << std::endl << "BOUNDS";
+	int tCounter = 1;
+	int fCounter = 1;
+
+	for (int i = 0; i < nr; ++i) {
+
+		for (std::vector<EventTermBound*>::iterator termsIt =
+				systems[i]->calculatedBounds.begin(); termsIt
+				!= systems[i]->calculatedBounds.end(); ++termsIt) {
+
+			if ((*termsIt)->upperBounded || (*termsIt)->lowerBounded) {
+
+			file << std::endl << "    F" << fCounter << ",T" << tCounter
+					<< ": ";
+			file << (*termsIt)->getLowerBoundString() << ","
+					<< (*termsIt)->getUpperBoundString() << ";";
+
+			}
+
+			++tCounter;
+		}
+
+		tCounter = 1;
+		++fCounter;
+	}
+
+	file << std::endl;
+
+}
+
 
 int main(int argc, char** argv) {
 
@@ -406,34 +544,19 @@ int main(int argc, char** argv) {
 
 	if (args_info.output_given) {
 
+		{
+			Output out( std::string(args_info.output_arg), "message profile" );
+			output(out, LindaAgent::getSystemsArray(), net, sysCounter);
+		}
+
 		if (std::string(args_info.output_arg) != std::string("-")) {
-			// Initialize stream
-			std::ofstream file;
-			file.open(args_info.output_arg);
-
-			// Create the file and output it to the stream
-			ProfileFile* outputFile = new ProfileFile(LindaAgent::getSystemsArray(), net,sysCounter);
-			outputFile->output(file);
-
-			// Close the file
-			file.close();
-			delete outputFile;
-
 			// Creation complete.
 			message("Output file %s created.", args_info.output_arg);
-
 		} else {
-
-			// Create the file and output it to the stream
-			ProfileFile* outputFile = new ProfileFile(LindaAgent::getSystemsArray(), net,sysCounter);
-			outputFile->output(std::cout);
-			delete outputFile;
-
-			std::cout << std::flush;
-
 			// Output complete.
 			message("Output written to standard out.");
 		}
+
 	} else {
 		message("Computation finished. No output produced, since no output target was given.");
 		message("To produce output to a file, use \"-o <path/to/file>\", to output to standard out, use \"-o-\". ");

263ace5e796e9a4b8cc1adeaaf4162c896d5ea35
Niels Lohmann <niels.lohmann@gmail.com>
2010-01-27 18:54:42 +0000

    * removed verbose.* functions from helper.* * added verbose.* to Makefile * made configure script less paranoid and trust the system that it provides me with some kind of working malloc (using autoscan's code is like wearing a tinhat...)

--- marlene/src/helper.h
+++ marlene/src/helper.h
@@ -24,6 +24,7 @@
 #include <string>
 #include "cmdline.h"
 #include "adapter.h"
+#include "verbose.h"
 
 // some initial values, which are the default values
 extern Adapter::ControllerType contType;
@@ -33,11 +34,8 @@ extern bool veryverbose;
 extern unsigned int veryverboseindent;
 
 extern gengetopt_args_info args_info;
-extern void message(const char* format, ...);
-extern void abort(unsigned short code, const char* format, ...);
 extern void evaluate_command_line(int argc, char* argv[]);
 
-extern void status(const char* format, ...);
 
 extern bool fileExists(std::string filename);
 

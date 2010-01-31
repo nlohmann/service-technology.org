6bb6522a6505935332099e47d5a93e2decff2dfd
Niels Lohmann <niels.lohmann@gmail.com>
2010-01-31 09:14:01 +0000

    * LoLA2Config is now compiled if configure script found a working libconfig++ * fixed bug #15223 (https://gna.org/bugs/?15223): COVER is now switched off in HOME mode

--- lola/utils/lola2config/main.cc
+++ lola/utils/lola2config/main.cc
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <libconfig.h++>
 #include "output_syntax.h"
+#include "config.h"
 
 using namespace libconfig;
 
@@ -11,6 +12,17 @@ Config cfg;
 
 int main(int argc, char **argv) {
     //Setting &deadlock = cfg.getRoot().add("deadlock", Setting::TypeGroup);
+
+    if (argc == 2 && !strcmp(argv[1], "--version")) {
+        printf("LoLA Result File Generator\n");
+        printf("  part of %s\n\n", PACKAGE_STRING);
+        return EXIT_SUCCESS;
+    }
+
+    if (argc == 2 && !strcmp(argv[1], "--help")) {
+        printf("call %s < FILE\n", argv[0]);
+        return EXIT_SUCCESS;
+    }
 
     output_parse();
     cfg.writeFile("foo.cfg");

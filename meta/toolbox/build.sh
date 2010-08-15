cdf671f1d04c7652ca882cbb8e83c6cd70f84119
Niels Lohmann <niels.lohmann@gmail.com>
2010-08-15 09:44:51 +0000

    * updated toolbox build script

--- meta/toolbox/build.sh
+++ meta/toolbox/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# avoid infinite loop
-rm configure.ac
+cd ../..
 
-DIRS=`find ../../ | grep "configure.ac" | grep -v ".svn" | sed -e 's/configure.ac//g;s/\/\//\//'`
+# avoid infinite loop
+rm -f configure.ac
+
+DIRS=`find . | grep "configure.ac" | grep -v ".svn" | sed -e 's/configure.ac//g;s/\/\//\//;s/.\///'`
+
+echo $DIRS
 
 mkdir build
 touch NEWS README AUTHORS ChangeLog
 
-echo "AC_INIT([service-technology.org toolbox], `svn info | grep "Revision" | awk '{print $2}'`, tools@service-technology.org, service-tech, http://service-technology.org)" > configure.ac
+echo "AC_INIT([service-technology.org toolbox], r`svn info | grep "Revision" | awk '{print $2}'`, tools@service-technology.org, service-tech, http://service-technology.org)" > configure.ac
 echo "AM_INIT_AUTOMAKE" >> configure.ac
 echo "AC_CONFIG_FILES(Makefile)" >> configure.ac
 echo -n "AC_CONFIG_SUBDIRS([" >> configure.ac
@@ -27,11 +31,10 @@ echo "	rm -fr AUTHORS COPYING ChangeLog INSTALL Makefile Makefile.in NEWS \\" >>
 echo "		README aclocal.m4 autom4te.cache config.log config.status \\" >> Makefile.am
 echo "		configure install-sh missing configure.ac Makefile.am" >> Makefile.am
 
-autoreconf -is -Wnone
+autoreconf -isv -Wnone
 ./configure --prefix=`pwd`/build --docdir=`pwd`/build/doc sysconfdir=`pwd`/build/bin
 
-make
-make -k install
-make -k install-html install-pdf AM_MAKEINFOHTMLFLAGS="--no-split"
+make -k -i all install install-html install-pdf AM_MAKEINFOHTMLFLAGS="--no-split"
 
+make -k -i dist
 make svn-clean

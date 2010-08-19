5e5c6ed1d706181127b00e644a0e52230564038f
Niels Lohmann <niels.lohmann@gmail.com>
2010-08-19 14:00:23 +0000

    fixed Windows problems

--- lola/src/update_configs.sh
+++ lola/src/update_configs.sh
@@ -22,6 +22,6 @@ do
   printf "bin_%s_SOURCES = \$(lola_SOURCES)\n" $ESCAPEDNAME >> Makefile.configs
   printf "bin_%s_CPPFLAGS = \$(AM_CPPFLAGS)\n" $ESCAPEDNAME >> Makefile.configs
 
-  printf "lola-%s:\n\t\$(AM_V_at)\$(MAKE) bin-lola-%s \$(AM_MAKEFLAGS) DEFAULT_INCLUDES=\"-I \$(srcdir)/configs/%s \$(DEFAULT_INCLUDES)\" CPPFLAGS=-DSTANDARDCONFIG\n" $FILE $FILE $FILE >> Makefile.configs
-  printf "\t\$(AM_V_GEN)mv \$(builddir)/bin-lola-%s \$(builddir)/lola-%s\n\n" $FILE $FILE >> Makefile.configs
+  printf "lola-%s:\n\t\$(AM_V_at)\$(MAKE) bin-lola-%s\$(EXEEXT) \$(AM_MAKEFLAGS) DEFAULT_INCLUDES=\"-I \$(srcdir)/configs/%s \$(DEFAULT_INCLUDES)\" CPPFLAGS=-DSTANDARDCONFIG\n" $FILE $FILE $FILE >> Makefile.configs
+  printf "\t\$(AM_V_GEN)mv \$(builddir)/bin-lola-%s\$(EXEEXT) \$(builddir)/lola-%s\$(EXEEXT)\n\n" $FILE $FILE >> Makefile.configs
 done

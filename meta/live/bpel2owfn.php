946eed87d237cc26a81f6b082124d50917701a99
stephan <stephan_mail@example.com>
2010-09-07 20:31:04 +0000

    * adjusted files.php to new front-end variables * follow-up adjustments to BPEL2oWFN (do not work yet)

--- meta/live/bpel2owfn.php
+++ meta/live/bpel2owfn.php
@@ -2,41 +2,32 @@
   if (empty($_REQUEST))
     header('Location:index.html#bpel2owfn');
 
+  // TODO: Either given from hidden fields or hacked like this!
+  $_REQUEST["toolname"] = "bpel2owfn";
+  $_REQUEST["fileextension"] = "bpel";
+
+  require 'resource/php/files.php';
   include 'resource/php/console.php';
 
-  // case: service was chosen from drop down menu
-  if (!strcmp($_REQUEST['input'], 'given')) {
-    $inputfilename = $_REQUEST['input_service'];
-  }
-
-  // case: service is given as URL -- download it
-  if (!strcmp($_REQUEST['input'], 'url')) {
-    $inputfilename = 'downloadedfile_'.rand().rand();
-    $download_command = 'wget \''.$_REQUEST['input_url'].'\' -O bpel2owfn/'.$inputfilename.'.bpel';
-    system($download_command);
-  }
+  foreach ($fileinfo as $file)
+  {
     
-  // case: service was uploaded by user
-  if (!strcmp($_REQUEST['input'], 'file')) {
-    $inputfilename = 'uploadedfile_'.rand().rand();
-    move_uploaded_file($_FILES['input_file']['tmp_name'], 'bpel2owfn/'.$inputfilename.'.bpel');
-  }
+    echo $file["residence"];
 
-
-  if (!strcmp($_REQUEST['output'], 'result')) {
-    $call = 'bpel2owfn -i bpel2owfn/'.$inputfilename.'.bpel -m petrinet -p '.$_REQUEST['patterns'].' -f '.$_REQUEST['format'].' -r '.$_REQUEST['reduce'];
-//    $call = $call.((isset($_REQUEST['reduce']))?' -p reduce':'');
-
-    header("Content-type: text/plain");
-    system($call);
-    return;
-  } else {
-    header("Content-Type: text/html");
-    echo '<?xml version="1.0" encoding="ISO-8859-1" ?>';
-    
-    $call = 'bpel2owfn -i '.$inputfilename.'.bpel -m petrinet -p '.$_REQUEST['patterns'].' -f '.$_REQUEST['format'].' -r '.$_REQUEST['reduce'].' -f dot -o';
-//    $call = $call.((isset($_REQUEST['reduce']))?' -p reduce':'');
-  }
+    if (!strcmp($_REQUEST['output'], 'result')) {
+      $call = 'bpel2owfn -i '.$file["residence"].' -m petrinet -p '.$_REQUEST['patterns'].' -f '.$_REQUEST['format'].' -r '.$_REQUEST['reduce'];
+  //    $call = $call.((isset($_REQUEST['reduce']))?' -p reduce':'');
+  
+      header("Content-type: text/plain");
+      system($call);
+      return;
+    } else {
+      header("Content-Type: text/html");
+      echo '<?xml version="1.0" encoding="ISO-8859-1" ?>';
+      
+      $call = 'bpel2owfn -i '.$file["residence"].' -m petrinet -p '.$_REQUEST['patterns'].' -f '.$_REQUEST['format'].' -r '.$_REQUEST['reduce'].' -f dot -o '.$file["short"].".dot";
+  //    $call = $call.((isset($_REQUEST['reduce']))?' -p reduce':'');
+    }
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -60,12 +51,12 @@
   <h2>Parameters</h2>
 
   <ul>
-    <li><strong>input file:</strong> <?=$inputfilename?>.bpel
+    <li><strong>input file:</strong> <?=$file["basename"]?>
       <?php
-        if (!strcmp($_REQUEST['input'], 'url'))
-          echo ' (downloaded from <a href="'.$_REQUEST['input_url'].'">'.$_REQUEST['input_url'].'</a>)';
-        if (!strcmp($_REQUEST['input'], 'file'))
-          echo ' (uploaded from local file '.$_FILES['input_file']['name'].')';
+        if (!strcmp($_REQUEST['input_type'], 'url'))
+          echo ' (downloaded from <a href="'.$file["link"].'">'.$file["link"].'</a>)';
+        if (!strcmp($_REQUEST['input_type'], 'file'))
+          echo ' (uploaded from local file '.$file["basename"].')';
       ?>
     </li>
     <li><strong>patterns:</strong> <?=$_REQUEST['patterns']?></li>
@@ -80,7 +71,7 @@
   <?php console($call, 'cd bpel2owfn; '.$call); ?>
   
 <?php
-  $thumbnailcall = 'convert -transparent "rgb(255,255,255)" -resize 200x200 bpel2owfn/'.$inputfilename.'.png bpel2owfn/'.$inputfilename.'.thumb.png';
+  $thumbnailcall = 'convert -transparent "rgb(255,255,255)" -resize 200x200 '.$file["short"].'.png '.$file["short"].'.thumb.png';
   system($thumbnailcall);
 ?>
 
@@ -88,26 +79,26 @@
   <table> 
   <?php
     echo '<tr>';
-    echo '<td><img src="resource/images/'.$_REQUEST['input'].'.png" height="100" width="100"></td>';
+    echo '<td><img src="'.$file["short"].'.png" height="100" width="100"></td>';
     echo '<td><img src="resource/images/arrow.png" height="96" width="96"></td>';
     echo '<td><img src="resource/images/tool.png" height="100" width="100"></td>';
-    echo '<td><img src="resource/images/arrow.png" height="96" width="96"></td>';
-    echo '<td><img src="resource/images/doc.png" height="100" width="100" style="margin-left:-30px;"><img src="bpel2owfn/'.$inputfilename.'.thumb.png" height="80" width="50" style="margin-bottom: 10px; margin-left: -80px;"></td>';
+    echo '<td><img src="resource/images/arrow.png" height="96residence" width="96"></td>';
+    echo '<td><img src="resource/images/doc.png" height="100" width="100" style="margin-left:-30px;"><img src="'.$file["short"].'.thumb.png" height="80" width="50" style="margin-bottom: 10px; margin-left: -80px;"></td>';
     echo '</tr>';
 
     echo '<tr>';
     echo '<td style="width: 150px;">';
-    if (!strcmp($_REQUEST['input'], 'url'))
-      echo '<a href="'.$_REQUEST['input_url'].'">downloaded file</a>';
-    if (!strcmp($_REQUEST['input'], 'given'))
-      echo '<a href="bpel2owfn/'.$_REQUEST['input_service'].'.bpel">BPEL process</a>';
-    if (!strcmp($_REQUEST['input'], 'file'))
-      echo '<a href="bpel2owfn/'.$inputfilename.'.bpel"/>'.$_FILES['input_file']['name'].'</a>';
+    if (!strcmp($_REQUEST['input_type'], 'url'))
+      echo '<a href="'.$file["link"].'">downloaded file</a>';
+    if (!strcmp($_REQUEST['input_type'], 'given'))
+      echo '<a href="'.$file["residence"].'">BPEL process</a>';
+    if (!strcmp($_REQUEST['input_type'], 'uploaded'))
+      echo '<a href="'.$file["residence"].'"/>'.$_FILES['input_file']['name'].'</a>';
     echo '</td>';
     echo '<td></td>';
     echo '<td style="width: 150px;">BPEL2oWFN</td>';
     echo '<td></td>';
-    echo '<td style="width: 150px;"><a href="bpel2owfn/'.$inputfilename.'.'.$_REQUEST['format'].'"/>'.((!strcmp($_REQUEST['format'], "owfn"))?'Fiona open net':'LoLA Petri net').'</a></td>';
+    echo '<td style="width: 150px;"><a href="'.$file["residence"].'.'.$_REQUEST['format'].'"/>'.((!strcmp($_REQUEST['format'], "owfn"))?'Fiona open net':'LoLA Petri net').'</a></td>';
     echo '</tr>';
   ?>
   </table>
@@ -115,13 +106,18 @@
   
   <h2>Output</h2>
   <p>
-    <a href="bpel2owfn/<?=$inputfilename?>.png" target="_blank"><img src="bpel2owfn/<?=$inputfilename?>.thumb.png" />
+    <a href="<?=$file["short"]?>.png" target="_blank"><img src="<?=$file["short"]?>.thumb.png" />
   </p>
   <p>
-    <a href="bpel2owfn/<?=$inputfilename?>.<?=$_REQUEST['format']?>">result</a>
+    <a href="<?=$file["short"]?>.<?=$_REQUEST['format']?>">result</a>
   </p>
   
   </div>
   </div>
 </body>
 </html>
+<?php 
+
+  }
+
+?>
--- meta/live/resource/php/files.php
+++ meta/live/resource/php/files.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'session.php';
+
 /**
  * Gets an array of file names, copies the files to the working
  * directory and returns an structured array with information on the 
@@ -55,28 +57,6 @@ function prepareFiles($fileArray)
 
   return $result;
 }
- /**
-  * Similar to prepareFiles, but instead of copying an array of files,
-  * one single file is created.
-  * Return value as above.
-  */
-function createFile($file)
-{
-  $result = array();
-  
-  // returns "basename", "extension", "dirname", and "filename"
-  $info = pathinfo($file);
-    
-  $result[$file] = $info;
-  $result[$file]["residence"] = $_SESSION["dir"]."/".$info["basename"];
-  $result[$file]["link"] = getLink($info["basename"]);
-   
-  $handle = fopen($result[$file]["residence"], "w+");
-  fwrite($handle, "");
-  fclose($handle);
-
-  return $result;
-}
 
 // create a string representing the URI for downloading a file
 function getLink($file)
@@ -111,4 +91,118 @@ function drawImage($file)
   echo '</div>';
 
 }
+
+/*
+ * 
+ */
+function exampleFile($example, $basedir)
+{
+  $result = array();
+  $info = pathinfo($example);
+  
+  $result[$example] = $info;
+  $result[$example]["residence"] = $_SESSION["dir"]."/".$info["basename"];
+  $result[$example]["short"] = $_SESSION["dir"]."/".$info["filename"];
+  $result[$example]["link"] = getLink($info["basename"]);
+  
+  copy($basedir."/".$example, $result[$example]["residence"]);
+  
+  return $result;
+}
+
+/*
+ * Creating a file from a given URL. Return value as above.
+ */
+function createFileFromUrl($url)
+{
+  $result = array();
+  
+  $info = pathinfo($url);
+  
+  $result[$url] = $info;
+  $result[$url]["residence"] = $_SESSION["dir"]."/".$info["basename"];
+  $result[$url]["short"] = $_SESSION["dir"]."/".$info["filename"];
+  $result[$url]["link"] = $url;
+  
+  $download = $download = 'wget \''.$url.'\' -O '.$result[$url]["residence"];
+  system($download);
+  
+  return $result;
+}
+
+/*
+ * 
+ */
+function createFileFromUpload()
+{
+  $result = array();
+  
+  $upload = 'uploadedfile_'.rand().rand();
+  $info = pathinfo($upload);
+  
+  $result[$upload] = $info;
+  $result[$file]["residence"] = $_SESSION["dir"]."/".$info["basename"];
+  $result[$file]["short"] = $_SESSION["dir"]."/".$info["filename"];
+  $result[$file]["link"] = getLink($info["basename"]); // FIXME
+    
+  move_uploaded_file($_FILES['input_file']['tmp_name'], $result[$upload]["residence"]);
+  
+  return $result;
+}
+
+/**
+* Similar to prepareFiles, but instead of copying an array of files,
+* one single file is created.
+* Return value as above.
+*/
+function createFile($file, $content = "")
+{
+  $result = array();
+  
+  // returns "basename", "extension", "dirname", and "filename"
+  $info = pathinfo($file);
+    
+  $result[$file] = $info;
+  $result[$file]["residence"] = $_SESSION["dir"]."/".$info["basename"];
+  $result[$file]["short"] = $_SESSION["dir"]."/".$info["filename"];
+  $result[$file]["link"] = getLink($info["basename"]); // FIXME
+   
+  $handle = fopen($result[$file]["residence"], "w+");
+  fwrite($handle, $content);
+  fclose($handle);
+
+  return $result;
+}
+
+/*
+ * Main part: This file MUST be included into any tool script. We process the given request variable
+ * to set or create files respectively.
+ */
+
+$fileinfo = array();
+
+// in case an example file was chosen from drop-down
+if ($_REQUEST["input_type"] == 'example')
+{
+  $fileinfo = exampleFile($_REQUEST["input_example"].'.'.$_REQUEST["fileextension"], $_REQUEST["toolname"]);
+}
+
+// in case an URL was given
+if ($_REQUEST["input_type"] == 'url')
+{
+  $fileinfo = createFileFromUrl($_REQUEST["input_url"]);
+}
+
+// in case a file was uploaded
+if ($_REQUEST["input_type"] == 'uploaded')
+{
+  $fileinfo = createFileFromUpload($_REQUEST["input_uploaded"]);
+}
+
+// in case a file was given
+if ($_REQUEST["input_type"] == 'given')
+{
+  $fileinfo = createFile('givenfile_'.rand().rand(), $_REQUEST["input_given"]);
+}
+
 ?>

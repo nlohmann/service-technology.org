c181b15ffc05a3a8603b70f9a1c7cf6b4b656a7b
Richard Müller <github@richardmueller.net>
2010-09-08 14:11:38 +0000

    documentation

--- candy/src/main.cc
+++ candy/src/main.cc
@@ -1,3 +1,23 @@
+/*****************************************************************************\
+ Candy -- Synthesizing cost efficient partners for services
+
+ Copyright (c) 2010 Richard MŸller
+
+ Candy is free software: you can redistribute it and/or modify it under the
+ terms of the GNU Affero General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option)
+ any later version.
+
+ Candy is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+ more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Candy.  If not, see <http://www.gnu.org/licenses/>.
+\*****************************************************************************/
+
+
 // global headers and DEBUG INFO END macros
 #include "settings.h"
 #include "Graph.h"

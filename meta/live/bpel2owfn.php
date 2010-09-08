0dd5853816872692ce2b341a8edc79284e836f20
Chris <gierds@informatik.hu-berlin.de>
2010-09-08 15:03:41 +0000

    *removed debug output

--- meta/live/bpel2owfn.php
+++ meta/live/bpel2owfn.php
@@ -14,11 +14,9 @@
   $realcall = "bpel2owfn";
   $fakeresult = "";
 
-  echo $_SESSION["bpel2owfn"];
-  $bla = $_SESSION["bpel2owfn"];
-  echo $process;
-  $blubb = $process[$bla];
-  $fakecall .= " -i ".$blubb["basename"];
+  $inputfile = $_SESSION["bpel2owfn"];
+  $file = $process[$inputfile];
+  $fakecall .= " -i ".$file["basename"];
   $realcall .= " ".$process[$_SESSION["bpel2owfn"]]["residence"];
   
   $fakecall .= " -m petrinet -p ".$_SESSION["patterns"]." -r ".$_SESSION["reduce"]." -f ".$_SESSION["format"];

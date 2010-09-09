be82ce1eb388daec6028788c71069c0dcf7621e9
Niels Lohmann <niels.lohmann@gmail.com>
2010-09-09 05:36:30 +0000

    * added more tools to the live bin

--- meta/live/update.sh
+++ meta/live/update.sh
@@ -20,13 +20,13 @@ wget http://service-technology.org/files/lola/lola.tar.gz
 tar xfz $TOOL.tar.gz
 cd $TOOL*
 ./configure --prefix=$DIR --sysconfdir=$DIR/bin
-make lola-statespace lola-statespace1
+make lola-statespace lola-statespace1 lola-bpm-liveprop1 lola-deadtransition lola-findpath lola-bpm-statepredicate1 lola-liveprop lola-boundedplace lola-boundednet lola-statepredicate
 make install
 cd ..
 rm -fr $TOOL* $TOOL.tar.gz
 
 
-for TOOL in pnapi wendy marlene mia bpel2owfn rachel x2dot
+for TOOL in pnapi wendy marlene mia bpel2owfn rachel x2dot compilers sayo uml2owfn
 do
   wget http://esla.informatik.uni-rostock.de:8080/job/$TOOL/lastSuccessfulBuild/artifact/$TOOL/$TOOL.tar.gz
   tar xfz $TOOL.tar.gz

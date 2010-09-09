904940a79af78227d27a402cdd74978c9468e3fc
Niels Lohmann <niels.lohmann@gmail.com>
2010-09-09 09:11:44 +0000

    * typo

--- meta/live/rachel.php
+++ meta/live/rachel.php
@@ -8,7 +8,7 @@
   require_once 'resource/php/files.php';
 
   // prepare example
-  switch () {
+  switch ($_SESSION['rachel']['input_example']) {
     case "rachel/running_example.owfn":
       $automata = array("rachel/running_example.sa");
       $og = array("rachel/running_example.og");

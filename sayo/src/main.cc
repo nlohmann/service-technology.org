af386ebb27e4f115b2d2d0b76fcf092201be380f
Niels Lohmann <niels.lohmann@gmail.com>
2011-07-19 08:45:49 +0000

    * try to remove a compiler warning

--- sayo/src/main.cc
+++ sayo/src/main.cc
@@ -84,7 +84,7 @@ void terminationHandler() {
     if (args_info.stats_flag) {
         fprintf(stderr, "%s: runtime: %.2f sec\n", PACKAGE, (static_cast<double>(clock()) - static_cast<double>(start_clock)) / CLOCKS_PER_SEC);
         fprintf(stderr, "%s: memory consumption: ", PACKAGE);
-        system((std::string("ps -o rss -o comm | ") + TOOL_GREP + " " + PACKAGE + " | " + TOOL_AWK + " '{ if ($1 > max) max = $1 } END { print max \" KB\" }' 1>&2").c_str());
+        (void)system((std::string("ps -o rss -o comm | ") + TOOL_GREP + " " + PACKAGE + " | " + TOOL_AWK + " '{ if ($1 > max) max = $1 } END { print max \" KB\" }' 1>&2").c_str());
     }
 }
 

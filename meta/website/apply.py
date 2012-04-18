3a017ccd2522164f5096ed589a1df58dd8eb96b7
suermeli <suermeli_mail@example.com>
2012-04-18 07:59:20 +0000

    added a JSON file to store people keyed by their gna username. integrated data into the get involved template etc

--- meta/website/apply.py
+++ meta/website/apply.py
@@ -4,12 +4,14 @@ import sys
 import json
 
 if (len(sys.argv) < 4):
-    sys.stderr.write("Usage: " + sys.argv[0] + " jsonfile template target" + "\n")
+    sys.stderr.write("Usage: " + sys.argv[0] + " jsonfile peoplefile template target" + "\n")
     sys.exit(1)
 
 j = json.loads(open(sys.argv[1], 'r').read())
-t = open(sys.argv[2], 'r').read()
-f = open(sys.argv[3], 'w')
+p = json.loads(open(sys.argv[2], 'r').read())
+
+t = open(sys.argv[3], 'r').read()
+f = open(sys.argv[4], 'w')
 
 if (len(j['authors'])) == 1:
     authors = j['authors'][0]
@@ -28,13 +30,21 @@ for thank in j['thanks']:
     thanks = thanks + '<li>' + thank + '</li>' + "\n"
 
 contributors = ''
+contribImages = ''
 for cont in j['commits']: 
-    contributors = contributors + '<li>' + cont['user'] + " (" + str(cont['commits'])
+    contributors = contributors + '<li>' + p['data'][cont['user']]['name'] + " (" + str(cont['commits'])
+    contribImages = contribImages + '<img width="75" src="../people/g/' + cont['user'] + '.jpg" class="portrait"> '
     if cont['commits'] > 1:
         contributors = contributors + " commits"
     else: 
         contributors = contributors + " commit"
     contributors = contributors + ")" + '</li>' + "\n"
+
+people = ''
+for username in p['people']:
+    people = people + '<li><img src="g/' + username + '.jpg" height="150" class="portrait"><br><a href="' + p['data'][username]['url'] + '">' + p['data'][username]['name'] + '</a><br> ' + p['data'][username]['affiliation'] + '</li>'
+
+
 
 t = t.replace('@SHORTNAME@', j['shortname'])
 t = t.replace('@TOOLNAME@',  j['toolname'])
@@ -49,5 +59,7 @@ t = t.replace('@LICENSE@', j['license'])
 t = t.replace('@RUNTIME@', j['runtime'])    
 t = t.replace('@OFFICIALVERSION@', j['officialVersion'])    
 t = t.replace('@CONTRIBUTORS@', contributors)
+t = t.replace('@CONTRIBUTORIMAGES@', contribImages)
+t = t.replace('@PEOPLE@', people)
 f.write(t)
 f.close()

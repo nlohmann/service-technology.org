3a7c300e400e35a2879591bf3e33c0fb15dfd460
Niels Lohmann <niels.lohmann@gmail.com>
2012-05-10 14:08:10 +0000

    * commented and tidied up (still broken) rewrite rules

--- lola2/src/Core/main.cc
+++ lola2/src/Core/main.cc
@@ -209,6 +209,7 @@ int main(int argc, char** argv)
 
                 TheFormula = TheFormula->rewrite(kc::arrows);
                 TheFormula = TheFormula->rewrite(kc::neg);
+                TheFormula = TheFormula->rewrite(kc::leq);
                 TheFormula = TheFormula->rewrite(kc::sides);
                 TheFormula = TheFormula->rewrite(kc::lists);
 

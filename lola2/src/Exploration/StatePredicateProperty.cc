120c915979c71108114a0eeb3ebfc45f36803153
karstenwolf <karstenwolf_mail@example.com>
2012-05-10 07:52:45 +0000

    dito

---
+++ lola2/src/Exploration/StatePredicateProperty.cc
@@ -0,0 +1,113 @@
+/*!
+\file StatePredicateProperty.cc
+\author Karsten
+\status new
+
+\brief Evaluates state predicate
+Actual property is virtual, default (base class) is full exploration
+*/
+
+
+#include "Exploration/StatePredicateProperty.h"
+#include "Formula/StatePredicate.h"
+#include "Formula/AtomicStatePredicate.h"
+#include "Net/Net.h"
+#include "Net/Transition.h"
+
+StatePredicateProperty::StatePredicateProperty(StatePredicate * f)
+{
+	// set formula
+	predicate = f;
+
+	index_t cardAtomic = f -> countAtomic();
+	AtomicStatePredicate ** atomic = (AtomicStatePredicate **) malloc(cardAtomic * SIZEOF_VOIDP);	
+	f -> collectAtomic(atomic);
+
+	cardChanged = (index_t *) calloc(Net::Card[TR],SIZEOF_INDEX_T);
+	changedPredicate = (AtomicStatePredicate ***) malloc(SIZEOF_VOIDP * Net::Card[TR]);
+	changedSum = (index_t **) malloc(SIZEOF_VOIDP * Net::Card[TR]);
+	for(index_t t = 0; t < Net::Card[TR]; t++)
+	{
+		changedPredicate[t] = (AtomicStatePredicate **) malloc(SIZEOF_VOIDP * cardAtomic);
+		changedSum[t] = (index_t *) malloc(SIZEOF_INDEX_T * cardAtomic);
+		for(index_t i = 0; i < cardAtomic; i++)
+		{
+			int s = 0;
+			// evaluate delta that t causes on this predicate
+			for(index_t j = 0; j < Transition::CardDeltaT[PRE][t]; j++)
+			{
+				const index_t p = Transition::DeltaT[PRE][t][j];
+				for(index_t k = 0; k < atomic[i]->cardPos;k++)
+				{
+					if(atomic[i]->posPlaces[k] == p)
+					{
+						s -= Transition::MultDeltaT[PRE][t][j] * atomic[i]->posMult[k];
+						break;
+						
+					}
+				}
+				for(index_t k = 0; k < atomic[i]->cardNeg;k++)
+				{
+					if(atomic[i]->negPlaces[k] == p)
+					{
+						s += Transition::MultDeltaT[PRE][t][j] * atomic[i]->negMult[k];
+						break;
+						
+					}
+				}
+			}
+			for(index_t j = 0; j < Transition::CardDeltaT[POST][t]; j++)
+			{
+				const index_t p = Transition::DeltaT[POST][t][j];
+				for(index_t k = 0; k < atomic[i]->cardPos;k++)
+				{
+					if(atomic[i]->posPlaces[k] == p)
+					{
+						s += Transition::MultDeltaT[POST][t][j] * atomic[i]->posMult[k];
+						break;
+						
+					}
+				}
+				for(index_t k = 0; k < atomic[i]->cardNeg;k++)
+				{
+					if(atomic[i]->negPlaces[k] == p)
+					{
+						s -= Transition::MultDeltaT[POST][t][j] * atomic[i]->negMult[k];
+						break;
+						
+					}
+				}
+			}
+			if(s)
+			{
+				changedPredicate[t][cardChanged[t]] = atomic[i];
+				changedSum[t][cardChanged[t]++] = s;
+			}
+		}
+		changedPredicate[t] = (AtomicStatePredicate **) realloc(changedPredicate[t],SIZEOF_VOIDP * cardChanged[t]);
+		changedSum[t] = (index_t *) realloc(changedSum[t], SIZEOF_INDEX_T * cardChanged[t]);
+	}
+	free(atomic);
+}
+		
+void StatePredicateProperty::initProperty()
+{
+	predicate -> evaluate();
+}
+
+void StatePredicateProperty::checkProperty(index_t t) 
+{
+	for(index_t i = 0; i < cardChanged[t]; i++)
+	{
+		changedPredicate[t][i]->update(changedSum[t][i]);
+	}
+}
+
+void StatePredicateProperty::updateProperty(index_t t) 
+{
+	for(index_t i = 0; i < cardChanged[t]; i++)
+	{
+		changedPredicate[t][i]->update(-changedSum[t][i]);
+	}
+}
+
---
+++ lola2/src/Exploration/StatePredicateProperty.h
@@ -0,0 +1,48 @@
+/*!
+\file StatePredicateProperty.h
+\author Karsten
+\status new
+
+\brief Evaluates simple property (only SET of states needs to be computed).
+Actual property is virtual, default (base class) is full exploration
+*/
+
+#pragma once
+
+#include "Formula/AtomicStatePredicate.h"
+#include "Exploration/SimpleProperty.h"
+
+class Firelist;
+class Store;
+
+class StatePredicateProperty: public SimpleProperty
+{
+
+    public:
+	StatePredicateProperty(StatePredicate *);
+    private:
+        /// prepare for search
+        virtual void initProperty();
+
+        /// check property in Marking::Current, use after fire. Argument is transition just fired.
+        virtual void checkProperty(index_t);
+
+        /// check property in Marking::Current, use after backfire. Argument is transition just backfired.
+        virtual void updateProperty(index_t);
+
+	// the actual formula to be verified; 
+	StatePredicate * predicate;
+
+	// for each transition t, number of state predicates that need to be checked
+	// when t is fired
+	index_t * cardChanged;
+
+	// for each transition t, an array with all state predicates that 
+	// need to be checked
+	// when t is fired
+	AtomicStatePredicate *** changedPredicate;
+
+	// changedSum[t][i] is the difference that t causes in the formal sum of 
+	// state predicate changedPredicate[t][i]
+	index_t ** changedSum;
+};

4ebc4071487bb36a260418ed0f5f623d535defa8
karstenwolf <karstenwolf_mail@example.com>
2012-05-10 08:08:33 +0000

    make Jenkins happy regarding malloc

--- lola2/src/Exploration/StatePredicateProperty.cc
+++ lola2/src/Exploration/StatePredicateProperty.cc
@@ -8,6 +8,7 @@ Actual property is virtual, default (base class) is full exploration
 */
 
 
+#include <cstdlib>
 #include "Exploration/StatePredicateProperty.h"
 #include "Formula/StatePredicate.h"
 #include "Formula/AtomicStatePredicate.h"

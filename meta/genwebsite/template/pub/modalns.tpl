5ae055d3bbd837dca967ab792aa71cbfea9642e1
suermeli <suermeli_mail@example.com>
2012-10-31 14:35:48 +0000

    Added keys instead of numbers, added a noscript fallback for publication info

---
+++ meta/genwebsite/template/pub/modalns.tpl
@@ -0,0 +1,5 @@
+<div id="@MODALNAME@">
+    <strong>@TITLE@</strong><br>
+    <i class="icon-user"></i> @AUTHORS@ <br>
+    <i class="icon-book"></i> @BOOK@, <em>@ADDITIONAL@</em>
+</div>
--- meta/genwebsite/utils/pub.py
+++ meta/genwebsite/utils/pub.py
@@ -9,28 +9,30 @@ import json
 def listThis(someString):
     return '<li>' + someString + '</li>' + "\n"
 
-def linkThis(someString, papercounter):
-  return '<a href="#paper-modal-' + str(papercounter) + '" data-toggle="modal">' + someString + '</a>'
+def linkThis(someString, key):
+  return '<a href="#paper-modal-' + key + '" data-toggle="modal">' + someString + '</a>'
   
     
-def makeModal(entry, template, papercounter):
+def makeModal(entry, template):
   result = template
   result = result.replace('@BOOK@', entry['book'])
   result = result.replace('@AUTHORS@', entry['authors'])
   result = result.replace('@ADDITIONAL@', entry['additional'])
   result = result.replace('@TITLE@', entry['title'])
-  result = result.replace('@MODALNAME@', "paper-modal-" + str(papercounter))
+  result = result.replace('@MODALNAME@', "paper-modal-" + entry['key'])
   return result
     
 # INITIALIZATION
 
-if (len(sys.argv) < 4):
-    sys.stderr.write("Usage: " + sys.argv[0] + " pubfile template modaltemplate target" + "\n")
+if (len(sys.argv) < 5):
+    sys.stderr.write("Usage: " + sys.argv[0] + " pubfile template modaltemplate noscripttemplate target" + "\n")
     sys.exit(1)
 s = open(sys.argv[1], 'r').read()
 print(s)
 p = json.loads(s)
 m = open(sys.argv[3], 'r').read()
+mns = open(sys.argv[4], 'r').read()
+
 replDict = dict();
 
 # BUILDING THE REPLACEMENT DICTIONARY
@@ -40,12 +42,11 @@ replDict = dict();
 replDict['paper-list-unstructured'] = ''
 replDict['paper-modals'] = ''
 
-papercounter = 1
 
 for paper in p['publications']:
-  replDict['paper-modals'] = replDict['paper-modals'] + makeModal(paper,m,papercounter)
-  replDict['paper-list-unstructured'] = replDict['paper-list-unstructured'] + listThis(paper['authors'] + ". " + linkThis("<strong>" + paper['title'] + "</strong>",papercounter))
-  papercounter = papercounter +1 
+  replDict['paper-modals'] = replDict['paper-modals'] + makeModal(paper,m)
+  replDict['paper-modals-noscript'] = replDict['paper-modals'] + makeModal(paper,mns)
+  replDict['paper-list-unstructured'] = replDict['paper-list-unstructured'] + listThis(paper['authors'] + ". " + linkThis("<strong>" + paper['title'] + "</strong>",paper['key']))
   
 
 # READ AND CLOSE TEMPLATE FILE
@@ -56,7 +57,7 @@ tfile.close()
 
 # REPLACE ALL OCCURRENCES IN THE TARGET FILE 
 
-f = open(sys.argv[4], 'w')
+f = open(sys.argv[5], 'w')
 
 for someKey in replDict: 
     t = t.replace('@' + someKey.upper() + '@', replDict[someKey])    

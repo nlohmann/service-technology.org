3fc890a82cb63e338c09789839a1c2c55d168720
Niels Lohmann <niels.lohmann@gmail.com>
2013-06-12 10:01:50 +0000

    * removed debugging code

--- megan2/src/Task.cc
+++ megan2/src/Task.cc
@@ -47,7 +47,7 @@ void Task::solve() {
     if (worker) {
         // call worker->execute()
         solution = std::async(&Tool::execute, worker);
-        message("worker started");
+        status("worker started");
     }
 }
 
